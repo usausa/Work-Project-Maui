fa326b8098b7c7380d79b9698a802fb066d37ded
usausa <machi.pon@gmail.com>
2018-06-05 16:50:45 +0900

    Update library

--- Example/Example.FormsApp/Example.FormsApp/Modules/Menu1View.xaml.cs
+++ Example/Example.FormsApp/Example.FormsApp/Modules/Menu1View.xaml.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace Example.FormsApp.Modules
+{
+    using Smart.Navigation.Attributes;
+
+    [View(ViewId.Menu1)]
+    public partial class Menu1View
+    {
+        public Menu1View()
+        {
+            InitializeComponent();
+        }
+    }
+}
