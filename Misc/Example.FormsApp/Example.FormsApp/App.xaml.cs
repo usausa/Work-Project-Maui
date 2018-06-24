c2e81865876e70f0c4175bcac5bff6601f90a136
usausa <machi.pon@gmail.com>
2018-06-24 17:00:19 +0900

    Update project baseline

--- Misc/Example.FormsApp/Example.FormsApp/App.xaml.cs
+++ Misc/Example.FormsApp/Example.FormsApp/App.xaml.cs
@@ -2,13 +2,61 @@
 
 namespace Example.FormsApp
 {
+    using System.Reflection;
+
+    using Example.FormsApp.Modules;
+
+    using Smart.Forms.Components;
+    using Smart.Forms.Resolver;
+    using Smart.Navigation;
+    using Smart.Resolver;
+
     public partial class App
     {
+        private readonly Navigator navigator;
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // Config Resolver
+            var resolver = CreateResolver();
+            ResolveProvider.Default.UseSmartResolver(resolver);
+
+            // Config Navigator
+            navigator = new NavigatorConfig()
+                .UseFormsNavigationProvider()
+                .UseResolver(resolver)
+                .UseIdViewMapper(m => m.AutoRegister(Assembly.GetExecutingAssembly().ExportedTypes))
+                .ToNavigator();
+            navigator.Navigated += (sender, args) =>
+            {
+                // for debug
+                System.Diagnostics.Debug.WriteLine(
+                    $"Navigated: [{args.Context.FromId}]->[{args.Context.ToId}] : stacked=[{navigator.StackedCount}]");
+            };
+
+            // Show MainWindow
+            MainPage = resolver.Get<MainPage>();
+
+            navigator.Forward(ViewId.Menu);
+        }
+
+        private SmartResolver CreateResolver()
+        {
+            var config = new ResolverConfig()
+                .UseAutoBinding()
+                .UseArrayBinding()
+                .UseAssignableBinding()
+                .UsePropertyInjector();
+
+            config.Bind<INavigator>().ToMethod(kernel => navigator).InSingletonScope();
+
+            config.Bind<IDialogService>().To<DialogService>().InSingletonScope();
+
+            config.Bind<ApplicationState>().ToSelf().InSingletonScope();
+
+            return config.ToResolver();
         }
 
         protected override void OnStart()
---
+++ Misc/Example.FormsApp/Example.FormsApp/ShellKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Example.FormsApp
+{
+    public enum ShellKeys
+    {
+        Back,
+        Function1,
+        Function2,
+        Function3,
+        Function4
+    }
+}
