b138dd85fbfacf874bdc86ddac8817594f848d06
usausa <machi.pon@gmail.com>
2019-01-11 21:30:59 +0900

    Add platform action

--- Business/Business.FormsApp/Business.FormsApp/Modules/Platform/PlatformActionViewModel.cs
+++ Business/Business.FormsApp/Business.FormsApp/Modules/Platform/PlatformActionViewModel.cs
@@ -1,20 +1,47 @@
 ﻿namespace Business.FormsApp.Modules.Platform
 {
+    using System;
     using System.Threading.Tasks;
 
     using Smart.Forms.Input;
     using Smart.Navigation;
 
+    using Xamarin.Essentials;
+
     public class PlatformActionViewModel : AppViewModelBase
     {
+        public AsyncCommand VibrateCommand { get; }
+        public AsyncCommand SpeakCommand { get; }
+
         public PlatformActionViewModel(ApplicationState applicationState)
             : base(applicationState)
         {
+            VibrateCommand = MakeAsyncCommand(Vibrate);
+            SpeakCommand = MakeAsyncCommand(Speak);
         }
 
         protected override Task OnNotifyBackAsync()
         {
             return Navigator.ForwardAsync(ViewId.PlatformMenu);
+        }
+
+        private Task Vibrate()
+        {
+            try
+            {
+                Vibration.Vibrate();
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private async Task Speak()
+        {
+            await TextToSpeech.SpeakAsync("Hello World");
         }
     }
 }
