1e55628505d3210c2339bc1636cdce4e6d5d6650
usausa <machi.pon@gmail.com>
2019-02-21 11:00:22 +0900

    Add slot example to sandbox

---
+++ DataMap/Sandbox/Slot/SlotExample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SlotExample
+{
+    public static class SlotManager
+    {
+        private static readonly object Sync = new object();
+
+        private static int next;
+
+        public static int Allocate()
+        {
+            lock (Sync)
+            {
+                next++;
+                return next;
+            }
+        }
+    }
+
+    public class Extension1
+    {
+        private static readonly int Slot;
+
+        static Extension1()
+        {
+            Slot = SlotManager.Allocate();
+        }
+
+        public int GetSlot()
+        {
+            return Slot;
+        }
+    }
+
+    public class Extension2
+    {
+        private static readonly int Slot;
+
+        static Extension2()
+        {
+            Slot = SlotManager.Allocate();
+        }
+
+        public int GetSlot()
+        {
+            return Slot;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var extension2 = new Extension2();
+            var extension1 = new Extension1();
+            Console.WriteLine(extension1.GetSlot());
+            Console.WriteLine(extension2.GetSlot());
+        }
+    }
+}
