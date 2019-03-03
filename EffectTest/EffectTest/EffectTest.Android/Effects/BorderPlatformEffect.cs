0ec1ce1e853d0f50f97e423a751b88fac1d2c177
usausa <machi.pon@gmail.com>
2019-03-03 19:41:25 +0900

    Add effect work

---
+++ EffectTest/EffectTest/EffectTest.Android/Effects/BorderPlatformEffect.cs
@@ -0,0 +1,120 @@
+﻿[assembly: Xamarin.Forms.ExportEffect(typeof(EffectTest.Droid.Effects.BorderPlatformEffect), nameof(EffectTest.Effects.BorderEffect))]
+
+namespace EffectTest.Droid.Effects
+{
+    using System.ComponentModel;
+
+    using Android.Graphics.Drawables;
+
+    using EffectTest.Effects;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Platform.Android;
+
+    public sealed class BorderPlatformEffect : PlatformEffect
+    {
+        private Android.Views.View view;
+
+        private Drawable oldDrawable;
+
+        private GradientDrawable border;
+
+        private Android.Graphics.Color color;
+
+        private int width;
+
+        private float radius;
+
+        protected override void OnAttached()
+        {
+            view = Container ?? Control;
+            oldDrawable = view.Background;
+            border = new GradientDrawable();
+
+            UpdateWidth();
+            UpdateColor();
+            UpdateRadius();
+            UpdateBorder();
+        }
+
+        protected override void OnDetached()
+        {
+            if (oldDrawable != null)
+            {
+                view.Background = oldDrawable;
+                oldDrawable = null;
+
+                view.SetPadding(0, 0, 0, 0);
+                view.ClipToOutline = false;
+            }
+
+            border?.Dispose();
+            border = null;
+            view = null;
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == BorderEffect.WidthProperty.PropertyName)
+            {
+                UpdateWidth();
+                UpdateBorder();
+            }
+            else if (args.PropertyName == BorderEffect.ColorProperty.PropertyName)
+            {
+                UpdateColor();
+                UpdateBorder();
+            }
+            else if (args.PropertyName == BorderEffect.RadiusProperty.PropertyName)
+            {
+                UpdateRadius();
+                UpdateBorder();
+            }
+        }
+
+        private void UpdateWidth()
+        {
+            width = (int)view.Context.ToPixels(BorderEffect.GetWidth(Element));
+        }
+
+        private void UpdateColor()
+        {
+            color = BorderEffect.GetColor(Element).ToAndroid();
+        }
+
+        private void UpdateRadius()
+        {
+            radius = view.Context.ToPixels(BorderEffect.GetRadius(Element));
+        }
+
+        private void UpdateBorder()
+        {
+            border.SetStroke(width, color);
+            border.SetCornerRadius(radius);
+
+            if (Element is BoxView boxView)
+            {
+                var backgroundColor = boxView.Color;
+                if (backgroundColor != Color.Default)
+                {
+                    border.SetColor(backgroundColor.ToAndroid());
+                }
+            }
+            else
+            {
+                var backgroundColor = ((View)Element).BackgroundColor;
+                if (backgroundColor != Color.Default)
+                {
+                    border.SetColor(backgroundColor.ToAndroid());
+                }
+            }
+
+            view.SetPadding(width, width, width, width);
+            view.ClipToOutline = true;
+
+            view.SetBackground(border);
+        }
+    }
+}
---
+++ EffectTest/EffectTest/EffectTest/Effects/BorderEffect.cs
@@ -0,0 +1,86 @@
+﻿namespace EffectTest.Effects
+{
+    using System.Linq;
+
+    using Xamarin.Forms;
+
+    public sealed class BorderEffect : RoutingEffect
+    {
+        public static readonly BindableProperty WidthProperty =
+            BindableProperty.CreateAttached(
+                "Width",
+                typeof(double),
+                typeof(BorderEffect),
+                default(double),
+                propertyChanged: OnWidthPropertyChanged);
+
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.CreateAttached(
+                "Color",
+                typeof(Color),
+                typeof(BorderEffect),
+                Color.Transparent);
+
+        public static readonly BindableProperty RadiusProperty =
+            BindableProperty.CreateAttached(
+                "Radius",
+                typeof(double),
+                typeof(BorderEffect),
+                default(double));
+
+        public static void SetWidth(BindableObject view, double value)
+        {
+            view.SetValue(WidthProperty, value);
+        }
+
+        public static double GetWidth(BindableObject view)
+        {
+            return (double)view.GetValue(WidthProperty);
+        }
+
+        public static void SetColor(BindableObject view, Color value)
+        {
+            view.SetValue(ColorProperty, value);
+        }
+
+        public static Color GetColor(BindableObject view)
+        {
+            return (Color)view.GetValue(ColorProperty);
+        }
+
+        public static void SetRadius(BindableObject view, double value)
+        {
+            view.SetValue(RadiusProperty, value);
+        }
+
+        public static double GetRadius(BindableObject view)
+        {
+            return (double)view.GetValue(RadiusProperty);
+        }
+
+        private static void OnWidthPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is VisualElement element))
+            {
+                return;
+            }
+
+            var effect = element.Effects.OfType<BorderEffect>().FirstOrDefault();
+            if (effect != null)
+            {
+                element.Effects.Remove(effect);
+            }
+
+            var width = (double)newValue;
+            if (width > 0)
+            {
+                element.Effects.Add(new BorderEffect());
+            }
+        }
+
+        public BorderEffect()
+            : base("Test.BorderEffect")
+        {
+        }
+    }
+}
