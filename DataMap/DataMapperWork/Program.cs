def5503452896c0488a7a8be637df114bcddd7d1
usausa <machi.pon@gmail.com>
2019-03-10 18:39:03 +0900

    Update work project

--- DataMap/DataMapperWork/Program.cs
+++ DataMap/DataMapperWork/Program.cs
@@ -1,9 +1,20 @@
 namespace DataMapperWork
 {
+    using Microsoft.Data.Sqlite;
+
+    using Smart.Data.Mapper;
+
     public static class Program
     {
         public static void Main(string[] args)
         {
+            using (var con = new SqliteConnection("Data Source=:memory:"))
+            {
+                con.Open();
+                con.Execute("CREATE TABLE IF NOT EXISTS Table1 (Id int PRIMARY KEY, Data text)");
+
+                con.Execute("INSERT INTO Table1 (Id, Data) VALUES (@Id, @Data)", new { Id = 1, Data = "test" });
+            }
         }
     }
 }
