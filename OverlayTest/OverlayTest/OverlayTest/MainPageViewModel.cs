f5f2857cf4ce99bd7cd69238b597400d03ad48e9
usausa <machi.pon@gmail.com>
2019-03-11 15:45:46 +0900

    Add overlay test

---
+++ OverlayTest/OverlayTest/OverlayTest/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace OverlayTest
+{
+    using Smart.ComponentModel;
+    using Smart.Forms.Input;
+    using Smart.Forms.ViewModels;
+
+    public class MainPageViewModel : ViewModelBase
+    {
+        public NotificationValue<bool> IsOverlay { get; } = new NotificationValue<bool>();
+
+        public DelegateCommand ShowCommand { get; }
+        public DelegateCommand CloseCommand { get; }
+
+        public MainPageViewModel()
+        {
+            ShowCommand = MakeDelegateCommand(() => IsOverlay.Value = true);
+            CloseCommand = MakeDelegateCommand(() => IsOverlay.Value = false);
+        }
+    }
+}
