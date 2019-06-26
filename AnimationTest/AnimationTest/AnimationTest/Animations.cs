5c32dd32396c5e48ef7e775f3b1fe4993a5a276e
usausa <machi.pon@gmail.com>
2019-06-26 12:32:49 +0900

    Update animation

---
+++ AnimationTest/AnimationTest/AnimationTest/Animations.cs
@@ -0,0 +1,19 @@
+﻿namespace AnimationTest
+{
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
+    public static class Animations
+    {
+        public static Task<bool> Slide(VisualElement element)
+        {
+            var width = element.Width;
+            var tcs = new TaskCompletionSource<bool>();
+            element.Animate("Slide", x =>
+            {
+
+            }, 16U, 1000U, Easing.Linear, (v, c) => tcs.SetResult(c));
+            return tcs.Task;
+        }
+    }
+}
--- AnimationTest/AnimationTest/AnimationTest/MainPage.xaml.cs
+++ AnimationTest/AnimationTest/AnimationTest/MainPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace AnimationTest
@@ -11,7 +9,7 @@ namespace AnimationTest
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
         private readonly List<View> views = new List<View>();
 
@@ -23,9 +21,50 @@ namespace AnimationTest
         private void Button1_OnClicked(object sender, EventArgs e)
         {
             ClearViews();
-            // TODO Animation
-            AddView(new BoxView { BackgroundColor = Color.OrangeRed });
-            AddView(new BoxView { BackgroundColor = Color.CornflowerBlue });
+
+            // TODO Animation基本機構
+            // - Task分離
+            // - 旧ビューの無効化、見た目？
+            // - ビューの分離とインタラクションがわかるように
+            // - 新ビューのアニメーション中イベント(Navigation自体が終わっていれば、発生しても問題ない？)
+            // - 分離ビュー更新
+            // - Taskが返ったとして、呼び出す側はawaitする？
+            // - Stack時、Enableの復帰、位置情報の復帰、Visibleの変更と気に
+            // - Navigationテストプロジェクト作成？
+
+            // アニメーションパターン網羅
+            // FadeIn     : Open   : addTop   , newView opacity 0 -> 1
+            // FadeOut    : Close  : addBottom, oldView opacity 1 -> 0
+            // SlideLeft  : Next   : addTop   , newView margin  100->0, old...
+            // SlideRight : Back   : addTop   , newView margin -100->0, old...
+            // TopDown    : Notify :
+            // TopUp      : Close  :
+            // BottomUp   : Notify :
+            // BottomDown : Close  :
+            //
+            // Scale : like fade ? Dialog? Scale and fade ? [center, 0% to 100%]
+            // Flip  : card (mode change?)
+            // Notify + opacity ?
+
+            var oldView = new BoxView { BackgroundColor = Color.OrangeRed };
+            AddView(oldView);
+            var newView = new BoxView { BackgroundColor = Color.CornflowerBlue };
+            AddView(newView);
+
+            var width = Container.Width;
+            newView.Animate("Slide", x =>
+            {
+                var oldMargin = (int)(x * width);
+                var newMargin = width - oldMargin;
+                Debug.WriteLine(oldMargin + " " + oldView.Width + " " + newMargin + " " + newView.Width);
+
+                oldView.Margin = new Thickness(-oldMargin, 0, oldMargin, 0);
+                newView.Margin = new Thickness(newMargin, 0, -newMargin, 0);
+
+            }, 16U, 2000U, Easing.Linear, (d, c) =>
+            {
+                Container.Children.Remove(oldView);
+            });
         }
 
         private void Button2_OnClicked(object sender, EventArgs e)
