355c491b015322e3a8085f188f9498fad9e78c86
usausa <machi.pon@gmail.com>
2020-05-22 14:58:27 +0900

    Update work

--- Apps/FeliCaReader.FormsApp/FeliCaReader.FormsApp/FeliCaReader.FormsApp/App.xaml.cs
+++ Apps/FeliCaReader.FormsApp/FeliCaReader.FormsApp/FeliCaReader.FormsApp/App.xaml.cs
@@ -15,17 +15,14 @@ namespace FeliCaReader.FormsApp
 
         protected override void OnStart()
         {
-            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
         }
     }
 }
