711c23ec9d59706b497f041e00bad12bc1238418
usausa <machi.pon@gmail.com>
2020-06-15 16:01:45 +0900

    Update

---
+++ Works/MultiBindingTest/MultiBindingTest/MultiBindingTest/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiBindingTest
+{
+    using Smart.ComponentModel;
+
+    public class MainPageViewModel
+    {
+        public NotificationValue<bool> IsChecked1 { get; } = new NotificationValue<bool>();
+
+        public NotificationValue<bool> IsChecked2 { get; } = new NotificationValue<bool>();
+    }
+}
