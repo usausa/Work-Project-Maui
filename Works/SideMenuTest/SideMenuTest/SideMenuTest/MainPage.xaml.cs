63b6085e418eef85750b523d471c0c220e7c34b9
usausa <machi.pon@gmail.com>
2020-09-26 21:35:37 +0900

    Update layout

--- Works/SideMenuTest/SideMenuTest/SideMenuTest/MainPage.xaml.cs
+++ Works/SideMenuTest/SideMenuTest/SideMenuTest/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,16 @@ namespace SideMenuTest
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PanGestureRecognizer_OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            Debug.WriteLine($"{e.StatusType} {e.TotalX} {e.TotalY}");
+        }
+
+        private void ImageButton_OnClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked");
         }
     }
 }
