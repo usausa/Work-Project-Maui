5c79ac692b984bc56dfd9047a07e1875c64e8b32
usausa <machi.pon@gmail.com>
2020-09-27 19:57:26 +0900

    Update

--- Works/SideMenuTest/SideMenuTest/SideMenuTest/MainPage.xaml.cs
+++ Works/SideMenuTest/SideMenuTest/SideMenuTest/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿namespace SideMenuTest
 {
     using System;
-    using System.Diagnostics;
 
     using Xamarin.Forms;
 
@@ -17,8 +16,6 @@
 
         private void Anchor_OnPanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            Debug.WriteLine($"{e.StatusType} {e.TotalX} {e.TotalY} {SideMenu.X},{SideMenu.Y},{SideMenu.Width},{SideMenu.Height}");
-
             if (e.StatusType == GestureStatus.Started)
             {
                 ViewExtensions.CancelAnimations(SideMenu);
@@ -29,39 +26,37 @@
             }
             else if (e.StatusType == GestureStatus.Running)
             {
-                //SideMenu.TranslateTo(pos, SideMenu.Y, 20);
                 var rect = AbsoluteLayout.GetLayoutBounds(SideMenu);
                 rect.X = Math.Max(Math.Min(0, e.TotalX - SideMenu.Width), -SideMenu.Width);
                 AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
 
-                showSideMenu = rect.X > lastPosition;
+                if (rect.X > lastPosition)
+                {
+                    showSideMenu = true;
+                }
+                else if (rect.X < lastPosition)
+                {
+                    showSideMenu = false;
+                }
+                //showSideMenu = e.TotalX > 0;
                 lastPosition = rect.X;
             }
             else if (e.StatusType == GestureStatus.Completed)
             {
                 var rect = AbsoluteLayout.GetLayoutBounds(SideMenu);
+                var pos = showSideMenu ? 0 : -SideMenu.Width;
+                var length = (uint)(250 * Math.Abs(pos - rect.X) / SideMenu.Width);
 
-                if (showSideMenu)
+                SideMenu.Animate("Slide", x =>
                 {
-                    //SideMenu.TranslateTo(0, SideMenu.Y, 250, Easing.SpringOut);
-                    rect.X = 0;
+                    rect.X = x;
                     AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
-                }
-                else
-                {
-                    //SideMenu.TranslateTo(-SideMenu.Width, SideMenu.Y, 250, Easing.SpringIn);
-                    rect.X = -SideMenu.Width;
-                    AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
-                }
+                }, rect.X, pos, length: length);
             }
-
-            Debug.WriteLine($"TranslateTo {AbsoluteLayout.GetLayoutBounds(SideMenu).X}");
         }
 
         private void SideMenu_OnPanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            Debug.WriteLine($"{e.StatusType} {e.TotalX} {e.TotalY} {SideMenu.X},{SideMenu.Y},{SideMenu.Width},{SideMenu.Height}");
-
             if (e.StatusType == GestureStatus.Started)
             {
                 ViewExtensions.CancelAnimations(SideMenu);
@@ -73,34 +68,32 @@
             else if (e.StatusType == GestureStatus.Running)
             {
                 var rect = AbsoluteLayout.GetLayoutBounds(SideMenu);
-                //SideMenu.TranslateTo(pos, SideMenu.Y, 20);
                 rect.X = Math.Max(Math.Min(0, rect.X + e.TotalX), -SideMenu.Width);
                 AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
 
-                showSideMenu = rect.X > lastPosition;
+                if (rect.X > lastPosition)
+                {
+                    showSideMenu = true;
+                }
+                else if (rect.X < lastPosition)
+                {
+                    showSideMenu = false;
+                }
+                //showSideMenu = e.TotalX > 0;
                 lastPosition = rect.X;
             }
             else if (e.StatusType == GestureStatus.Completed)
             {
                 var rect = AbsoluteLayout.GetLayoutBounds(SideMenu);
+                var pos = showSideMenu ? 0 : -SideMenu.Width;
+                var length = (uint)(250 * Math.Abs(pos - rect.X) / SideMenu.Width);
 
-                if (showSideMenu)
+                SideMenu.Animate("Slide", x =>
                 {
-                    //SideMenu.TranslateTo(0, SideMenu.Y, 250, Easing.SpringOut);
-                    rect.X = 0;
+                    rect.X = x;
                     AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
-                }
-                else
-                {
-                    //SideMenu.TranslateTo(-SideMenu.Width, SideMenu.Y, 250, Easing.SpringIn);
-                    rect.X = -SideMenu.Width;
-                    AbsoluteLayout.SetLayoutBounds(SideMenu, rect);
-
-                    showSideMenu = e.TotalX > 0;
-                }
+                }, rect.X, pos, length: length);
             }
-
-            Debug.WriteLine($"TranslateTo {AbsoluteLayout.GetLayoutBounds(SideMenu).X}");
         }
     }
 }
