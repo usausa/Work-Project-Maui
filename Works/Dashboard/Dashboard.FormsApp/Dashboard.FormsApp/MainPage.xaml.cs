90412f0e49a12ab43c48ff2305eb4fa97a5221f6
usausa <machi.pon@gmail.com>
2021-03-13 18:14:48 +0900

    Move to work

---
+++ Works/Dashboard/Dashboard.FormsApp/Dashboard.FormsApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace Dashboard.FormsApp
+{
+    using System.ComponentModel;
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
