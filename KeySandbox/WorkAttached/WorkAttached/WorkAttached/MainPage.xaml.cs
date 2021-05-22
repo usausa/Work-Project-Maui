196d37ce3cf4a0b091db55ebe430ba4e5f140d5e
usausa <machi.pon@gmail.com>
2021-05-22 16:03:45 +0900

    Add work project

---
+++ KeySandbox/WorkAttached/WorkAttached/WorkAttached/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace WorkAttached
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Container.Children.Clear();
+            Container.Children.Add(new View1());
+        }
+
+        private void MainPage_OnDescendantAdded(object sender, ElementEventArgs e)
+        {
+            Debug.WriteLine($"**** DescendantAdded {e.Element.GetType()}");
+        }
+
+        private void MainPage_OnDescendantRemoved(object sender, ElementEventArgs e)
+        {
+            Debug.WriteLine($"**** DescendantRemoved {e.Element.GetType()}");
+        }
+    }
+}
---
+++ KeySandbox/WorkAttached/WorkAttached/WorkAttached/Shortcut.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace WorkAttached
+{
+    using Xamarin.Forms;
+
+    public static class Shortcut
+    {
+        public static readonly BindableProperty KeyProperty = BindableProperty.CreateAttached(
+            "Key",
+            typeof(int),
+            typeof(Shortcut),
+            null,
+            propertyChanged: PropertyChanged);
+
+        public static int GetKey(BindableObject view)
+        {
+            return (int)view.GetValue(KeyProperty);
+        }
+
+        public static void SetKey(BindableObject view, int value)
+        {
+            view.SetValue(KeyProperty, value);
+        }
+
+        private static void PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            Debug.WriteLine($"**** PropertyChanged {bindable.GetType()} {newValue}");
+        }
+    }
+}
