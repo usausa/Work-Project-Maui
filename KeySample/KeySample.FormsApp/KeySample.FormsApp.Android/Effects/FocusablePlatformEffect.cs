c24d50a22c25e690edadb75349893936f431df3a
usausa <machi.pon@gmail.com>
2021-05-23 13:48:40 +0900

    Update effect

--- KeySample/KeySample.FormsApp/KeySample.FormsApp.Android/Effects/FocusablePlatformEffect.cs
+++ KeySample/KeySample.FormsApp/KeySample.FormsApp.Android/Effects/FocusablePlatformEffect.cs
@@ -2,13 +2,17 @@
 
 namespace KeySample.FormsApp.Droid.Effects
 {
+    using Xamarin.Forms;
     using Xamarin.Forms.Platform.Android;
 
     public sealed class FocusablePlatformEffect : PlatformEffect
     {
         protected override void OnAttached()
         {
-            Control.FocusableInTouchMode = true;
+            ((VisualElement)Element).FocusChangeRequested += (_, args) =>
+            {
+                args.Result = Control?.RequestFocus() ?? false;
+            };
         }
 
         protected override void OnDetached()
