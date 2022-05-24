c1e55bb954ad94379931ddda5a9dfc16e79135e2
Kenta watanabe <kwatana4@gw.nsw.co.jp>
2022-05-24 19:41:06 +0900

    Add project

---
+++ Example.MobileApp/Example.MobileApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Example.MobileApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Example.MobileApp/Example.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.MobileApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
