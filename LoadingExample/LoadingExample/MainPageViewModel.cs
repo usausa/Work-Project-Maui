16ed33264ebe58c6cd3dddfe28e526c0efbc2d93
usausa <machi.pon@gmail.com>
2022-06-13 15:40:40 +0900

    Update

---
+++ LoadingExample/LoadingExample/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace LoadingExample;
+
+using System.Windows.Input;
+
+using Smart.ComponentModel;
+using Smart.Maui.ViewModels;
+
+public class MainPageViewModel : ViewModelBase
+{
+    public NotificationValue<bool> Loading { get; } = new();
+
+    public ICommand LoadingCommand { get; }
+    public ICommand DummyCommand { get; }
+
+    public MainPageViewModel()
+    {
+        LoadingCommand = MakeAsyncCommand(Execute);
+        DummyCommand = MakeDelegateCommand(() => System.Diagnostics.Debug.WriteLine("*"));
+    }
+
+    private async Task Execute()
+    {
+        try
+        {
+            Loading.Value = true;
+
+            await Task.Delay(5000);
+        }
+        finally
+        {
+            Loading.Value = false;
+        }
+    }
+}
---
+++ LoadingExample/README.md
@@ -0,0 +1,4 @@
+# Problem
+
+* Cannot be disabled with opacity on Android
+* What to do when using popup ?
