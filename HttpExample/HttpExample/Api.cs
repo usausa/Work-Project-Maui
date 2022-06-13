6230d078a5dbc8cc71f67a7a7096182067c50279
usausa <machi.pon@gmail.com>
2022-06-13 13:15:36 +0900

    Add project baseline

---
+++ HttpExample/HttpExample/Api.cs
@@ -0,0 +1,18 @@
+﻿namespace HttpExample;
+
+public class ItemResponse
+{
+    public string Code { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+}
+
+public class EchoRequest
+{
+    public string Message { get; set; } = default!;
+}
+
+public class EchoResponse
+{
+    public string Message { get; set; } = default!;
+}
