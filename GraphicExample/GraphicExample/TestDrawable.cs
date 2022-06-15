1d8236cac03eafae7aa52593bd3e579e8b8c4de2
usausa <machi.pon@gmail.com>
2022-06-15 10:37:12 +0900

    Update

---
+++ GraphicExample/GraphicExample/TestDrawable.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphicExample;
+
+internal class TestDrawable : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        // Draw border
+        canvas.DrawRectangle(dirtyRect.Inflate(-1, -1));
+
+        canvas.FillColor = Colors.Black;
+        canvas.FillCircle(new Point(dirtyRect.Width / 2, dirtyRect.Height / 2), 15d);
+    }
+}
