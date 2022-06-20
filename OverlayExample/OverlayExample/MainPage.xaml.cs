b2d752755abb87399b4d2e315e0eaa06c69b27c9
usausa <machi.pon@gmail.com>
2022-06-20 16:51:01 +0900

    Update

--- OverlayExample/OverlayExample/MainPage.xaml.cs
+++ OverlayExample/OverlayExample/MainPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class MainPage
     {
         InitializeComponent();
     }
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine("**** Click start");
+
+        var window = GetParentWindow();
+        var loading = new LoadingOverlay(window);
+        window.AddOverlay(loading);
+
+        await Task.Delay(3000);
+
+        window.RemoveOverlay(loading);
+
+        System.Diagnostics.Debug.WriteLine("**** Click end");
+    }
 }
