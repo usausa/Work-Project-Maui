f67d4a650b3c06b52d3167f9bfa2bb4cfa435094
usausa <machi.pon@gmail.com>
2023-06-23 15:13:52 +0900

    Update work project

--- Works/WorkControl3/MauiProgram.cs
+++ Works/WorkControl3/MauiProgram.cs
@@ -1,25 +1,31 @@
-﻿using Microsoft.Extensions.Logging;
+﻿namespace WorkControl3;
 
-namespace WorkControl3
+using Microsoft.Extensions.Logging;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+	builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        // Customize only custom entry
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoKeyboardEntry", (handler, entry) =>
+        {
+#if ANDROID
+            handler.PlatformView.ShowSoftInputOnFocus = false;
+#endif
+        });
+        return builder.Build();
     }
 }
