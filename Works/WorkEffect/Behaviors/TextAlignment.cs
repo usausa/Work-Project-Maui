25bc4a1639e9cb35701fd57d9860e22d516ff623
usausa <machi.pon@gmail.com>
2023-06-28 14:42:34 +0900

    Update

---
+++ Works/WorkEffect/Behaviors/TextAlignment.cs
@@ -0,0 +1,58 @@
+﻿//namespace WorkEffect.Behaviors;
+
+//using Microsoft.Maui.Handlers;
+
+//public static class TextAlignment
+//{
+//    public static readonly BindableProperty OnProperty = BindableProperty.CreateAttached(
+//        "On",
+//        typeof(bool),
+//        typeof(TextAlignment),
+//        false);
+
+//    public static bool GetOn(BindableObject bindable) => (bool)bindable.GetValue(OnProperty);
+
+//    public static void SetOn(BindableObject bindable, bool value) => bindable.SetValue(OnProperty, value);
+
+//    public static readonly BindableProperty VerticalProperty =
+//        BindableProperty.CreateAttached(
+//            "Vertical",
+//            typeof(Microsoft.Maui.TextAlignment),
+//            typeof(TextAlignment),
+//            Microsoft.Maui.TextAlignment.Center);
+
+//    public static Microsoft.Maui.TextAlignment GetVertical(BindableObject bindable) =>
+//        (Microsoft.Maui.TextAlignment)bindable.GetValue(VerticalProperty);
+
+//    public static void SetVertical(BindableObject bindable, Microsoft.Maui.TextAlignment value) =>
+//        bindable.SetValue(VerticalProperty, value);
+
+//    public static readonly BindableProperty HorizontalProperty =
+//        BindableProperty.CreateAttached(
+//            "Horizontal",
+//            typeof(Microsoft.Maui.TextAlignment),
+//            typeof(TextAlignment),
+//            Microsoft.Maui.TextAlignment.Center);
+
+//    public static Microsoft.Maui.TextAlignment GetHorizontal(BindableObject bindable) =>
+//        (Microsoft.Maui.TextAlignment)bindable.GetValue(HorizontalProperty);
+
+//    public static void SetHorizontal(BindableObject bindable, Microsoft.Maui.TextAlignment value) =>
+//        bindable.SetValue(HorizontalProperty, value);
+
+//    public static void UseCustomMapper()
+//    {
+//#if ANDROID
+//        LabelHandler.Mapper.Add("On", (handler, _) =>
+//        {
+//            var element = (Label)handler.VirtualView;
+//            var on = GetOn(element);
+//            if (on)
+//            {
+//                handler.PlatformView.Gravity = GetVertical(element).ToVerticalGravity() |
+//                                               GetHorizontal(element).ToHorizontalGravity();
+//            }
+//        });
+//#endif
+//    }
+//}
---
+++ Works/WorkEffect/Behaviors/TextAlignmentExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace WorkEffect.Behaviors;
+
+using Android.Views;
+
+public static class TextAlignmentExtensions
+{
+    public static GravityFlags ToHorizontalGravity(this Microsoft.Maui.TextAlignment alignment)
+    {
+        return alignment switch
+        {
+            Microsoft.Maui.TextAlignment.Center => GravityFlags.CenterHorizontal,
+            Microsoft.Maui.TextAlignment.End => GravityFlags.Right,
+            Microsoft.Maui.TextAlignment.Start => GravityFlags.Left,
+            _ => throw new InvalidOperationException(alignment.ToString())
+        };
+    }
+
+    public static GravityFlags ToVerticalGravity(this Microsoft.Maui.TextAlignment alignment)
+    {
+        return alignment switch
+        {
+            Microsoft.Maui.TextAlignment.Center => GravityFlags.CenterVertical,
+            Microsoft.Maui.TextAlignment.End => GravityFlags.Bottom,
+            Microsoft.Maui.TextAlignment.Start => GravityFlags.Top,
+            _ => throw new InvalidOperationException(alignment.ToString())
+        };
+    }
+}
