8fe6f8ef8d1e8fd2ca1794531baa7c91480cc965
usausa <machi.pon@gmail.com>
2023-06-29 17:04:46 +0900

    Update

---
+++ Works/WorkFont/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkFont
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
