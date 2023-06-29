21acb8fc39e2a51d179c656d0fb4b43f9133b653
usausa <machi.pon@gmail.com>
2023-06-29 17:20:33 +0900

    Update

--- Works/WorkAdjust/App.xaml.cs
+++ Works/WorkAdjust/App.xaml.cs
@@ -1,12 +1,15 @@
-﻿namespace WorkAdjust
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+
+namespace WorkAdjust
 {
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
+            On<Microsoft.Maui.Controls.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
         }
     }
 }
