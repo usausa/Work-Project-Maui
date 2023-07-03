f64213a28d8bf3e06012bcf1ea710f3b7022af6d
usausa <machi.pon@gmail.com>
2023-07-03 10:53:50 +0900

    Add work project

---
+++ Works/WorkBackPressed/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WorkBackPressed;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    protected override bool OnBackButtonPressed()
+    {
+        System.Diagnostics.Debug.WriteLine("* OnBackButtonPressed");
+        return true;
+    }
+}
+
