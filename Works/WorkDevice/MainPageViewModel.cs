3a31b97bff62e7efebd172c32bf24f5dd911a2e1
usausa <machi.pon@gmail.com>
2023-07-06 13:47:58 +0900

    Update

---
+++ Works/WorkDevice/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Smart.ComponentModel;
+
+namespace WorkDevice;
+
+using Smart.Maui.ViewModels;
+
+public class MainPageViewModel : ViewModelBase
+{
+    public NotificationValue<string> Data { get; } = new();
+
+    public MainPageViewModel()
+    {
+        Data.Value = "123";
+
+        UpdateBatteryStatus();
+    }
+
+    private void UpdateBatteryStatus()
+    {
+    }
+}
