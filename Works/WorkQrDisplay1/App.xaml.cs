54010d770828893f8ddc52ced5e00cdce660e8db
usausa <machi.pon@gmail.com>
2023-07-15 21:12:09 +0900

    Update

---
+++ Works/WorkQrDisplay1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkQrDisplay1;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Works/WorkQrDisplay1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkQrDisplay1;
+
+using QRCoder;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        using var generator = new QRCodeGenerator();
+        using var data = generator.CreateQrCode("うさうさだよもん", QRCodeGenerator.ECCLevel.L, true);
+        var png = new PngByteQRCode(data);
+        var bytes = png.GetGraphic(20);
+        Image.Source = ImageSource.FromStream(() => new MemoryStream(bytes));
+    }
+}
+
