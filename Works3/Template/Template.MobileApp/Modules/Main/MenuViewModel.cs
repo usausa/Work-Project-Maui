a1a5de2e581f95c78c34cbd0a63e3bad8d43bea0
usausa <machi.pon@gmail.com>
2023-07-19 09:38:21 +0900

    Update

--- Works3/Template/Template.MobileApp/Modules/Main/MenuViewModel.cs
+++ Works3/Template/Template.MobileApp/Modules/Main/MenuViewModel.cs
@@ -4,12 +4,20 @@ using Template.MobileApp;
 
 public class MenuViewModel : AppViewModelBase
 {
+    public NotificationValue<string> Flavor { get; } = new();
+
+    public NotificationValue<Version> Version { get; } = new();
+
     public ICommand ForwardCommand { get; }
 
     public MenuViewModel(
-        ApplicationState applicationState)
+        ApplicationState applicationState,
+        IAppInfo appInfo)
         : base(applicationState)
     {
+        Flavor.Value = !String.IsNullOrEmpty(Variants.Flavor()) ? Variants.Flavor() : "Unknown";
+        Version.Value = appInfo.Version;
+
         ForwardCommand = MakeAsyncCommand<ViewId>(x => Navigator.ForwardAsync(x));
     }
 }
