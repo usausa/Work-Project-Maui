480783132bc46ad30bb0f1b66c9ec1b9acb92fee
usausa <machi.pon@gmail.com>
2023-08-27 12:18:46 +0900

    Update

--- Works/WorkSwipe/MainPage.xaml.cs
+++ Works/WorkSwipe/MainPage.xaml.cs
@@ -1,10 +1,17 @@
 ﻿namespace WorkSwipe;
 
+using System.Diagnostics;
+
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+    {
+        Debug.WriteLine("* Swiped");
     }
 }
 
