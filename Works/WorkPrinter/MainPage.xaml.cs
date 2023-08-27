bd9aecafebdf77a1e23c343684d43998c9eceba8
usausa <machi.pon@gmail.com>
2023-08-27 21:19:20 +0900

    Update

--- Works/WorkPrinter/MainPage.xaml.cs
+++ Works/WorkPrinter/MainPage.xaml.cs
@@ -6,5 +6,30 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private async void Button_OnClicked(object sender, EventArgs e)
+    {
+#if ANDROID
+        var result = await Permissions.CheckStatusAsync<BluetoothConnectPermission>();
+        if (result != PermissionStatus.Granted)
+        {
+            result = await Permissions.RequestAsync<BluetoothConnectPermission>();
+            if (result != PermissionStatus.Granted)
+            {
+                return;
+            }
+        }
+#endif
+    }
 }
 
+#if ANDROID
+internal class BluetoothConnectPermission : Permissions.BasePlatformPermission
+{
+    public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+    {
+        (Android.Manifest.Permission.BluetoothScan, true),
+        (Android.Manifest.Permission.BluetoothConnect, true)
+    }.ToArray();
+}
+#endif
