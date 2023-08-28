a3add1bdad272156731322fa741bf8a37441e144
usausa <machi.pon@gmail.com>
2023-08-28 17:42:29 +0900

    Update

--- Works/WorkPrinter/MainPage.xaml.cs
+++ Works/WorkPrinter/MainPage.xaml.cs
@@ -1,7 +1,20 @@
 ﻿namespace WorkPrinter;
 
-public partial class MainPage : ContentPage
+using System.Text;
+
+using Android.Bluetooth;
+using Android.Util;
+
+using Java.Util;
+
+using MauiComponents;
+
+using Microsoft.Maui.Controls;
+
+public partial class MainPage
 {
+    private static readonly UUID SppUuid = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB")!;
+
     public MainPage()
     {
         InitializeComponent();
@@ -9,27 +22,72 @@ public partial class MainPage : ContentPage
 
     private async void Button_OnClicked(object sender, EventArgs e)
     {
-#if ANDROID
-        var result = await Permissions.CheckStatusAsync<BluetoothConnectPermission>();
+        var result = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
         if (result != PermissionStatus.Granted)
         {
-            result = await Permissions.RequestAsync<BluetoothConnectPermission>();
-            if (result != PermissionStatus.Granted)
+            return;
+        }
+
+#if ANDROID
+        result = await Permissions.RequestAsync<BluetoothPermission>();
+        if (result != PermissionStatus.Granted)
+        {
+            return;
+        }
+
+        var helper = new BluetoothHelper(ActivityResolver.CurrentActivity);
+
+        //var enable = helper.IsAdapterEnabled();
+
+        var device = await helper.FindDeviceAsync(x => x.Name is not null && x.Name.Contains("DummyPrinter"));
+        if (device is null)
+        {
+            return;
+        }
+
+        if ((device.BondState != Bond.Bonded) && !await helper.BondAsync(device, Encoding.ASCII.GetBytes("8888")))
+        {
+            device = null;
+            return;
+        }
+
+        // TODO socket
+        // Execute
+        var socket = default(BluetoothSocket);
+        try
+        {
+            // TODO Timeout?
+            socket = device.CreateRfcommSocketToServiceRecord(SppUuid)!;
+
+            await socket.ConnectAsync();
+
+            // Write
+            var send = Encoding.ASCII.GetBytes("test");
+            await socket.OutputStream!.WriteAsync(send, 0, send.Length);
+
+            // TODO timeout, buffer ?
+            // Read
+            var receive = new byte[16];
+            var read = await socket.InputStream!.ReadAsync(receive, 0, receive.Length);
+            if (read <= 0)
             {
+                device = null;
                 return;
             }
+
+            var response = Encoding.ASCII.GetString(receive, 0, read);
+            System.Diagnostics.Debug.WriteLine(response);
+        }
+        catch (Java.IO.IOException ex)
+        {
+            Log.Error("DummyPrinter", ex, "Connection error.");
+            device = null;
+            return;
+        }
+        finally
+        {
+            socket?.Close();
         }
 #endif
     }
 }
-
-#if ANDROID
-internal class BluetoothConnectPermission : Permissions.BasePlatformPermission
-{
-    public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
-    {
-        (Android.Manifest.Permission.BluetoothScan, true),
-        (Android.Manifest.Permission.BluetoothConnect, true)
-    }.ToArray();
-}
-#endif
--- Works/WorkPrinter/MauiProgram.cs
+++ Works/WorkPrinter/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace WorkPrinter;
+﻿using CommunityToolkit.Maui;
+
+namespace WorkPrinter;
 using Microsoft.Extensions.Logging;
 
 public static class MauiProgram
@@ -8,6 +10,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
