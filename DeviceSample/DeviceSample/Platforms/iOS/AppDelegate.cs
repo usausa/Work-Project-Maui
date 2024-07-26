eb5f0517862352ee4deb1380615e317672ebe791
usausa <machi.pon@gmail.com>
2024-07-26 21:18:23 +0900

    Update baseline

--- DeviceSample/DeviceSample/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DeviceSample;
-using Foundation;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
