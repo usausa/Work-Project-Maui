6452fa24e52860d9899a13051f2a0356fb593fa0
usausa <machi.pon@gmail.com>
2024-08-10 18:36:49 +0900

    Update

--- Old/Camera/CameraSample/MainPage.xaml.cs
+++ Old/Camera/CameraSample/MainPage.xaml.cs
@@ -1,10 +1,34 @@
 namespace CameraSample;
 
+using System.Diagnostics;
+using CommunityToolkit.Maui.Views;
+
 public partial class MainPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        CameraImage.Source = null;
+        await CameraView.CaptureImage(CancellationToken.None);
+        Debug.WriteLine("* CaptureImage started.");
+    }
+
+    private void CameraView_OnMediaCaptured(object? sender, MediaCapturedEventArgs e)
+    {
+        Debug.WriteLine("* Media captured.");
+        Dispatcher.Dispatch(() =>
+        {
+            CameraImage.Source = ImageSource.FromStream(() => e.Media);
+        });
+    }
+
+    private void CameraView_OnMediaCaptureFailed(object? sender, MediaCaptureFailedEventArgs e)
+    {
+        Debug.WriteLine("* Media capture failed.");
     }
 }
 
