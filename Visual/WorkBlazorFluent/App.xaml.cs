f1d1ab526cede08c32356c716ab1efd678ba63a5
usausa <machi.pon@gmail.com>
2024-09-09 18:30:04 +0900

    Add project

---
+++ Visual/WorkBlazorFluent/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkBlazorFluent;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Visual/WorkBlazorFluent/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkBlazorFluent;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
