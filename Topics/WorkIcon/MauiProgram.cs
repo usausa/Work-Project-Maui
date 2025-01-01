bfed6649744526f542cd08ec00dd92ecf5a28259
usausa <machi.pon@gmail.com>
2025-01-01 12:54:09 +0900

    Update

---
+++ Topics/WorkIcon/MauiProgram.cs
@@ -0,0 +1,52 @@
+﻿using MauiIcons.Core;
+using MauiIcons.Fluent;
+using MauiIcons.Fluent.Filled;
+using MauiIcons.FontAwesome;
+using MauiIcons.FontAwesome.Brand;
+using MauiIcons.FontAwesome.Solid;
+using MauiIcons.Material;
+using MauiIcons.Material.Outlined;
+using MauiIcons.Material.Rounded;
+using MauiIcons.Material.Sharp;
+
+using Microsoft.Extensions.Logging;
+
+namespace WorkIcon
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiIconsCore(x =>
+                {
+                    x.SetDefaultIconSize(30.0);
+                    x.SetDefaultIconAutoScaling(true);
+                    x.SetDefaultFontOverride(true);
+                })
+                .UseMaterialMauiIcons()
+                .UseMaterialOutlinedMauiIcons()
+                .UseMaterialRoundedMauiIcons()
+                .UseMaterialSharpMauiIcons()
+                //.UseSegoeFluentMauiIcons()
+                .UseFluentMauiIcons()
+                .UseFluentFilledMauiIcons()
+                //.UseCupertinoMauiIcons()
+                .UseFontAwesomeMauiIcons()
+                .UseFontAwesomeSolidMauiIcons()
+                .UseFontAwesomeBrandMauiIcons()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
