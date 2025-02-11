dda220f920fed267fde8d85668d334b12e467bd2
usausa <machi.pon@gmail.com>
2025-02-11 15:33:42 +0900

    Update

---
+++ WorkButton/WorkButton/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MultiLineButton.Text = String.Join(Environment.NewLine, "CPU", "100%");
+        }
+    }
+}
