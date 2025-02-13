0dde1ab455586add41425f8e5d8a172f25448513
usausa <machi.pon@gmail.com>
2025-02-13 11:44:21 +0900

    Update

--- WorkButton/WorkButton/MainPage.xaml.cs
+++ WorkButton/WorkButton/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace WorkButton
+namespace WorkButton
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +7,20 @@
             InitializeComponent();
 
             MultiLineButton.Text = String.Join(Environment.NewLine, "CPU", "100%");
+        }
+
+        // ReSharper disable once AsyncVoidMethod
+        private async void SourceButton_OnClicked(object? sender, EventArgs e)
+        {
+            // [MEMO] svgだと動作しない？
+            //await using var stream = await FileSystem.OpenAppPackageFileAsync("folder.svg");
+            await using var stream = await FileSystem.OpenAppPackageFileAsync("folder2.png");
+            var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            // ReSharper disable once AccessToDisposedClosure
+            var source = ImageSource.FromStream(() => ms);
+            SourceButton.Source = source;
         }
     }
 }
