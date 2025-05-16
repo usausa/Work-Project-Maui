6615559bacb8fad4d053b34da20678d0561bfc61
usausa <machi.pon@gmail.com>
2025-05-16 11:06:22 +0900

    Update

--- Works3/WorkSmartMaui/WorkSmartMaui/MainPageViewModel.cs
+++ Works3/WorkSmartMaui/WorkSmartMaui/MainPageViewModel.cs
@@ -1,12 +1,25 @@
-using System.Diagnostics;
-
 namespace WorkSmartMaui;
 
+using System.Diagnostics;
 using System.Windows.Input;
 
-internal class MainPageViewModel : AppViewModelBase
+using Smart.Mvvm;
+using Smart.Mvvm.Messaging;
+
+internal partial class MainPageViewModel : AppViewModelBase
 {
+    public EventRequest<string> FocusRequest { get; } = new();
+
+    [ObservableProperty]
+    public partial bool Enable { get; set; }
+
     public ICommand BusyCommand { get; }
+
+    public ICommand EnableCommand { get; }
+
+    public ICommand Focus1Command { get; }
+
+    public ICommand Focus2Command { get; }
 
     public MainPageViewModel()
     {
@@ -15,6 +28,15 @@ internal class MainPageViewModel : AppViewModelBase
             Debug.WriteLine("* Start");
             await Task.Delay(5000);
             Debug.WriteLine("* End");
+        });
+
+        EnableCommand = MakeDelegateCommand(() => Enable = !Enable);
+
+        Focus1Command = MakeDelegateCommand(() => FocusRequest.Request("Target1"));
+        Focus2Command = MakeAsyncCommand(async () =>
+        {
+            await Task.Delay(1000);
+            FocusRequest.Request("Target2");
         });
     }
 }
