62bffa5399a4c861ebfae25456a385cfbba73071
usausa <machi.pon@gmail.com>
2025-05-24 21:13:55 +0900

    Update

--- Works3/Template/Template.MobileApp/Modules/AppViewModelBase.cs
+++ Works3/Template/Template.MobileApp/Modules/AppViewModelBase.cs
@@ -1,10 +1,16 @@
 namespace Template.MobileApp.Modules;
 
+using System.ComponentModel.DataAnnotations;
+
+using Smart.Mvvm.Resolver;
+
 using Template.MobileApp.Shell;
 
 [ObservableGeneratorOption(Reactive = true, ViewModel = true)]
-public abstract class AppViewModelBase : ExtendViewModelBase, INavigatorAware, INavigationEventSupport, INotifySupportAsync<ShellEvent>
+public abstract class AppViewModelBase : ExtendViewModelBase, IValidatable, INavigatorAware, INavigationEventSupport, INotifySupportAsync<ShellEvent>
 {
+    private List<ValidationResult>? validationResults;
+
     public INavigator Navigator { get; set; } = default!;
 
     protected override void Dispose(bool disposing)
@@ -12,6 +18,30 @@ public abstract class AppViewModelBase : ExtendViewModelBase, INavigatorAware, I
         base.Dispose(disposing);
 
         System.Diagnostics.Debug.WriteLine($"{GetType()} is Disposed");
+    }
+
+    // TODO BunnyTail version
+    public void Validate(string name)
+    {
+        var pi = GetType().GetProperty(name);
+        if (pi is null)
+        {
+            return;
+        }
+
+        validationResults ??= new List<ValidationResult>();
+
+        var value = pi.GetValue(this, null);
+        var context = new ValidationContext(this, DefaultResolveProvider.Default, null)
+        {
+            MemberName = name
+        };
+        if (!Validator.TryValidateProperty(value, context, validationResults))
+        {
+            Errors.AddError(name, validationResults[0].ErrorMessage!);
+        }
+
+        validationResults.Clear();
     }
 
     public virtual void OnNavigatingFrom(INavigationContext context)
--- Works3/Template/Template.MobileApp/Modules/Basic/BasicValidationViewModel.cs
+++ Works3/Template/Template.MobileApp/Modules/Basic/BasicValidationViewModel.cs
@@ -1,7 +1,33 @@
 namespace Template.MobileApp.Modules.Basic;
 
-public sealed class BasicValidationViewModel : AppViewModelBase
+public sealed partial class BasicValidationViewModel : AppViewModelBase
 {
+    public ValidationFocusRequest ValidationFocusRequest { get; } = new();
+
+    [Required(ErrorMessage = "Required")]
+    [ObservableProperty]
+    public partial string Text1 { get; set; } = default!;
+
+    [ObservableProperty]
+    public partial string Text2 { get; set; } = default!;
+
+    public ICommand ErrorCommand { get; }
+    public ICommand ClearCommand { get; }
+    public ICommand FocusCommand { get; }
+
+    public BasicValidationViewModel()
+    {
+        ErrorCommand = MakeDelegateCommand(() =>
+        {
+            Errors.AddError(nameof(Text2), "Manual error");
+        });
+        ClearCommand = MakeDelegateCommand(() =>
+        {
+            Errors.ClearErrors(nameof(Text2));
+        });
+        FocusCommand = MakeDelegateCommand(ValidationFocusRequest.FocusRequest);
+    }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.BasicMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
