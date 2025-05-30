d212adc9c5a1399284e86c7063181a5c6aca8f31
usausa <machi.pon@gmail.com>
2025-05-30 19:43:05 +0900

    Update

--- Works3/WorkMauiMisc/WorkNewCamera/MainPageViewModel.cs
+++ Works3/WorkMauiMisc/WorkNewCamera/MainPageViewModel.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
 using System.Windows.Input;
+using CommunityToolkit.Maui.Core.Primitives;
+using Smart;
+using Smart.Linq;
 
 namespace WorkNewCamera;
 
@@ -10,12 +13,12 @@ public class MainPageViewModel : ExtendViewModelBase
     public CameraController Controller { get; } = new();
 
     public ICommand InfoCommand { get; }
-    //public ICommand FlipCommand { get; }
-    //public ICommand FlashCommand { get; }
+    public ICommand ListCommand { get; }
+    public ICommand FlipCommand { get; }
+    public ICommand TorchCommand { get; }
+    public ICommand FlashCommand { get; }
     //public ICommand SizeCommand { get; }
     //public ICommand ZoomCommand { get; }
-    //public ICommand TorchCommand { get; }
-    public ICommand ListCommand { get; }
     //public ICommand StartCommand { get; }
     //public ICommand StopCommand { get; }
     //public ICommand CaptureCommand { get; }
@@ -46,5 +49,47 @@ public class MainPageViewModel : ExtendViewModelBase
             var list = await Controller.GetAvailableListAsync();
             Debug.WriteLine($"* Count: {list.Count}");
         });
+        FlipCommand = MakeAsyncCommand(async () =>
+        {
+            var list = await Controller.GetAvailableListAsync();
+            if (Controller.Selected is null)
+            {
+                Controller.Selected = list.FirstOrDefault();
+            }
+            else
+            {
+                var index = list.FindIndex(x => x.DeviceId == Controller.Selected.DeviceId);
+                if ((index < 0) || (index == list.Count - 1))
+                {
+                    Controller.Selected = list.FirstOrDefault();
+                }
+                else
+                {
+                    Controller.Selected = list[index + 1];
+                }
+
+            }
+        });
+        TorchCommand = MakeDelegateCommand(() => Controller.IsTorchOn = !Controller.IsTorchOn);
+        FlashCommand = MakeDelegateCommand(() =>
+        {
+            if (Controller.Selected?.IsFlashSupported ?? false)
+            {
+                switch (Controller.CameraFlashMode)
+                {
+                    case CameraFlashMode.Off:
+                        Controller.CameraFlashMode = CameraFlashMode.On;
+                        break;
+                    case CameraFlashMode.On:
+                        Controller.CameraFlashMode = CameraFlashMode.Auto;
+                        break;
+                    case CameraFlashMode.Auto:
+                        Controller.CameraFlashMode = CameraFlashMode.Off;
+                        break;
+                }
+            }
+        });
     }
 }
+
+// TODO Ext switch camera zoom up down
