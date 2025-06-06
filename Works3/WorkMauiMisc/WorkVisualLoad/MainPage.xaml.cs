41871f9b4756ce42eb4b5e2f9ddb0302b48fb556
usausa <machi.pon@gmail.com>
2025-06-06 11:20:03 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkVisualLoad/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+namespace WorkVisualLoad;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        //var loadavg = File.ReadAllText("/proc/loadavg");
+        //var port = new SerialPort("/dev/ttyACM0");
+        //port.Open();
+    }
+}
