bc13e2fde686a17683b1d1ea06201d10fe9b1aa6
usausa <machi.pon@gmail.com>
2025-06-11 16:27:27 +0900

    Update

--- Works3/WorkMauiMisc/WorkNfc/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkNfc/MainPage.xaml.cs
@@ -1,18 +1,31 @@
 namespace WorkNfc;
 
+using Android.App;
+using Android.Content;
+using Android.Nfc;
+using Android.Nfc.Tech;
+
+using Application = Android.App.Application;
+
 public partial class MainPage : ContentPage
 {
+    private readonly NfcReader reader;
+
     public MainPage()
     {
         InitializeComponent();
+
+        reader = new NfcReader();
     }
 
     private void OnStartClicked(object? sender, EventArgs e)
     {
+        reader.Start();
     }
 
     private void OnStopClicked(object? sender, EventArgs e)
     {
+        reader.Stop();
     }
 }
 
@@ -24,6 +37,68 @@ public partial class NfcReader
 #if ANDROID
 public partial class NfcReader
 {
-    // TODO
+    private NfcAdapter? nfcAdapter;
+
+    public void Start()
+    {
+        if (nfcAdapter is null)
+        {
+            var nfcManager = (NfcManager)Application.Context.GetSystemService(Context.NfcService)!;
+            nfcAdapter = nfcManager.DefaultAdapter!;
+        }
+
+        var techLists = new List<string[]>();
+        techLists.Add(["android.nfc.tech.NfcF"]);
+
+        var activity = ActivityResolver.CurrentActivity;
+
+        var intent = new Intent(activity, activity.GetType()).AddFlags(ActivityFlags.SingleTop);
+        nfcAdapter.EnableForegroundDispatch(
+            activity,
+            PendingIntent.GetActivity(activity, 0, intent, PendingIntentFlags.Mutable),
+            [new IntentFilter(NfcAdapter.ActionNdefDiscovered)],
+            techLists.ToArray());
+    }
+
+    public void Stop()
+    {
+        var activity = ActivityResolver.CurrentActivity;
+
+        nfcAdapter?.DisableForegroundDispatch(activity);
+    }
+
+    public static void ProcessIntent(Intent intent)
+    {
+        try
+        {
+            var idm = intent.GetByteArrayExtra(NfcAdapter.ExtraId)!;
+            // TODO
+            var tag = (Tag?)intent.GetParcelableExtra(NfcAdapter.ExtraTag);
+            if (tag is null)
+            {
+                return;
+            }
+
+            var list = tag.GetTechList()!;
+            if (list.Any(x => x == "android.nfc.tech.NfcF"))
+            {
+                var nfc = NfcF.Get(tag)!;
+                nfc.Timeout = 1000;
+                nfc.Connect();
+            }
+        }
+        catch (TagLostException)
+        {
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Debug.WriteLine(e);
+        }
+    }
+}
+
+public static class ActivityResolver
+{
+    public static Activity CurrentActivity { get; set; } = default!;
 }
 #endif
--- Works3/WorkMauiMisc/WorkNfc/MauiProgram.cs
+++ Works3/WorkMauiMisc/WorkNfc/MauiProgram.cs
@@ -1,5 +1,11 @@
-﻿namespace WorkNfc;
+namespace WorkNfc;
+
+using Android.Nfc;
+
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+using System.Diagnostics;
 
 public static class MauiProgram
 {
@@ -12,6 +18,41 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .ConfigureLifecycleEvents(events =>
+            {
+#if ANDROID
+                events.AddAndroid(android => android.OnCreate((activity, bundle) =>
+                {
+                    Debug.WriteLine("* Created 1");
+                }));
+#endif
+            })
+            .ConfigureLifecycleEvents(events =>
+            {
+#if ANDROID
+                events.AddAndroid(android =>
+                {
+                    android.OnCreate((activity, bundle) =>
+                    {
+                        ActivityResolver.CurrentActivity = activity;
+                        Debug.WriteLine("* Created 2");
+                    });
+                    android.OnResume(activity =>
+                    {
+                        Debug.WriteLine("* OnResume 2");
+                        ActivityResolver.CurrentActivity = activity;
+                    });
+                    android.OnNewIntent((activity, intent) =>
+                    {
+                        Debug.WriteLine($"* OnNewIntent {intent?.Action}");
+                        if (intent?.Action == NfcAdapter.ActionTechDiscovered)
+                        {
+                            NfcReader.ProcessIntent(intent);
+                        }
+                    });
+                });
+#endif
             });
 
 #if DEBUG
