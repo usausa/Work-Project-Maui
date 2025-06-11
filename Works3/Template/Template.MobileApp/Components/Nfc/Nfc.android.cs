3aa0daa6491524f8a0c16317d3de98edf28fb15e
usausa <machi.pon@gmail.com>
2025-06-12 08:51:10 +0900

    Update

--- Works3/Template/Template.MobileApp/Components/Nfc/Nfc.android.cs
+++ Works3/Template/Template.MobileApp/Components/Nfc/Nfc.android.cs
@@ -1,1 +1,32 @@
-//namespace Template.MobileApp.Components.Nfc;
+namespace Template.MobileApp.Components.Nfc;
+
+using Android.Nfc;
+using Android.Nfc.Tech;
+
+#pragma warning disable CA1819
+public sealed class AndroidNfcF : INfc
+{
+    private readonly NfcF nfc;
+
+    public byte[] Id { get; }
+
+    public AndroidNfcF(byte[] id, NfcF nfc)
+    {
+        Id = id;
+        this.nfc = nfc;
+    }
+
+    public byte[] Access(byte[] command)
+    {
+        try
+        {
+            var response = nfc.Transceive(command);
+            return response ?? [];
+        }
+        catch (TagLostException)
+        {
+            return [];
+        }
+    }
+}
+#pragma warning restore CA1819
