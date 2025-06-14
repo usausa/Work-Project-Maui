7f0021661b667b9ddb611d4ef8038d3bed1261bf
usausa <machi.pon@gmail.com>
2025-06-14 12:57:19 +0900

    Update

--- Works3/Template/Template.MobileApp/Components/Nfc/FeliCaExtensions.cs
+++ Works3/Template/Template.MobileApp/Components/Nfc/FeliCaExtensions.cs
@@ -1,5 +1,14 @@
 namespace Template.MobileApp.Components.Nfc;
 
+#pragma warning disable CA1819
+public sealed class ReadBlock
+{
+    public byte BlockNo { get; set; }
+
+    public byte[] BlockData { get; set; } = default!;
+}
+#pragma warning restore CA1819
+
 public static class FeliCaExtensions
 {
     public static byte[] ExecutePolling(this INfc nfc, short systemCode)
--- Works3/Template/Template.MobileApp/Components/Nfc/Nfc.cs
+++ Works3/Template/Template.MobileApp/Components/Nfc/Nfc.cs
@@ -3,13 +3,60 @@ namespace Template.MobileApp.Components.Nfc;
 #pragma warning disable CA1819
 public interface INfc
 {
+    byte[] Id { get; }
+
+    void SetTimeout(int timeout);
+
     byte[] Access(byte[] command);
 }
 #pragma warning restore CA1819
 
+public sealed class NfcEventArgs : EventArgs
+{
+    public INfc Tag { get; }
+
+    public NfcEventArgs(INfc tag)
+    {
+        Tag = tag;
+    }
+}
+
 public interface INfcReader
 {
-    public bool Enable { get; set; }
+    public event EventHandler<NfcEventArgs>? Detected;
 
-    IObservable<INfc> Discovered { get; }
+    public bool Enabled { get; set; }
+}
+
+public sealed partial class NfcReader : INfcReader
+{
+    public event EventHandler<NfcEventArgs>? Detected;
+
+    public bool Enabled
+    {
+        get;
+        set
+        {
+            if (value)
+            {
+                if (!field)
+                {
+                    Start();
+                    field = true;
+                }
+            }
+            else
+            {
+                if (field)
+                {
+                    Stop();
+                    field = false;
+                }
+            }
+        }
+    }
+
+    private partial void Start();
+
+    private partial void Stop();
 }
