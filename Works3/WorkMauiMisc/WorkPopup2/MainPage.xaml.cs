d749319d88b3d63ae09080a1d845c0ea603fb7a1
usausa <machi.pon@gmail.com>
2025-06-16 14:57:16 +0900

    Update

--- Works3/WorkMauiMisc/WorkPopup2/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkPopup2/MainPage.xaml.cs
@@ -1,4 +1,9 @@
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Extensions;
+
 namespace WorkPopup2;
+
+using Microsoft.Maui.Controls.Shapes;
 
 public partial class MainPage : ContentPage
 {
@@ -7,7 +12,20 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnTest1Click(object? sender, EventArgs e)
+    private async void OnTest1Click(object? sender, EventArgs e)
     {
+        await this.ShowPopupAsync(new Label
+        {
+            Text = "This is a very important message!"
+        }, new PopupOptions
+        {
+            CanBeDismissedByTappingOutsideOfPopup = true,
+            Shape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(20, 20, 20, 20),
+                StrokeThickness = 2,
+                Stroke = Colors.LightGray
+            }
+        });
     }
 }
--- Works3/WorkMauiMisc/WorkPopup2/MauiProgram.cs
+++ Works3/WorkMauiMisc/WorkPopup2/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace WorkPopup2;
+namespace WorkPopup2;
+using CommunityToolkit.Maui;
+
 using Microsoft.Extensions.Logging;
 
 public static class MauiProgram
@@ -8,6 +10,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
