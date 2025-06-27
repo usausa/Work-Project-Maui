2b52e519c5b63a2e31355e23853da46c8ef9130c
usausa <machi.pon@gmail.com>
2025-06-27 15:01:13 +0900

    Update

--- Works3/WorkMauiMisc/WorkRepeat/MainPageViewModel.cs
+++ Works3/WorkMauiMisc/WorkRepeat/MainPageViewModel.cs
@@ -1,7 +1,36 @@
 namespace WorkRepeat;
 
+using Smart.Maui.Input;
 using Smart.Maui.ViewModels;
+
+using System.Globalization;
+using System.Windows.Input;
 
 public class MainPageViewModel : ExtendViewModelBase
 {
+    private bool executing;
+
+    public IObserveCommand RepeatCommand { get; }
+
+    public MainPageViewModel()
+    {
+        RepeatCommand = MakeAsyncCommand(async () =>
+        {
+            RepeatCommand!.RaiseCanExecuteChanged();
+
+            if (executing)
+            {
+                System.Diagnostics.Debug.WriteLine("* B");
+                return;
+            }
+
+            executing = true;
+
+            System.Diagnostics.Debug.WriteLine("* S");
+            await Task.Delay(1000);
+            System.Diagnostics.Debug.WriteLine("* E");
+
+            executing = false;
+        }, () => !BusyState.IsBusy);
+    }
 }
