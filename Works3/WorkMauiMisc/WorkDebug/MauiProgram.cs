f2c82e557c8948deba1cf6c3c98cb884ba11445a
usausa <machi.pon@gmail.com>
2025-06-28 19:34:09 +0900

    Update

--- Works3/WorkMauiMisc/WorkDebug/MauiProgram.cs
+++ Works3/WorkMauiMisc/WorkDebug/MauiProgram.cs
@@ -1,13 +1,34 @@
-﻿namespace WorkDebug;
+using Plugin.Maui.DebugRainbows;
+
+namespace WorkDebug;
 using Microsoft.Extensions.Logging;
+
+using Plugin.Maui.DebugOverlay;
 
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
+        // https://github.com/davidortinau/Plugin.Maui.DebugOverlay
+        // https://github.com/sthewissen/Plugin.Maui.DebugRainbows
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseDebugRibbon(Colors.Orange)
+#if DEBUG
+            //.UseDebugRainbows()
+            .UseDebugRainbows(new DebugRainbowsOptions
+            {
+                ShowRainbows = true,
+                ShowGrid = true,
+                HorizontalItemSize = 20,
+                VerticalItemSize = 20,
+                MajorGridLineInterval = 4,
+                MajorGridLines = new GridLineOptions { Color = Color.FromRgb(255, 0, 0), Opacity = 0.5, Width = 3 },
+                MinorGridLines = new GridLineOptions { Color = Color.FromRgb(255, 0, 0), Opacity = 0.25, Width = 1 },
+                GridOrigin = DebugGridOrigin.TopLeft,
+            })
+#endif
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
