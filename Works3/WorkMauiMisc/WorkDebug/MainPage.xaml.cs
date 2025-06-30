7fdb0aa882f0f6efdd08f6c6e3b793a29d1fda12
usausa <machi.pon@gmail.com>
2025-06-30 14:55:43 +0900

    Update

--- Works3/WorkMauiMisc/WorkDebug/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDebug/MainPage.xaml.cs
@@ -1,9 +1,16 @@
 namespace WorkDebug;
 
+using System.Diagnostics;
+
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        Debug.WriteLine("* Clicked");
     }
 }
