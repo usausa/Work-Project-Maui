de93067d718fd4243f5d3cdde7c2530ad676958e
usausa <machi.pon@gmail.com>
2025-07-09 09:15:39 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkBluetooth/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+namespace WorkBluetooth;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnTestClicked(object? sender, EventArgs e)
+    {
+    }
+}
