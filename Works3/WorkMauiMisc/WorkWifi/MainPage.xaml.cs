fae94962eb690b81dfa8d7c1bd245c22316cfea0
usausa <machi.pon@gmail.com>
2025-07-11 09:05:39 +0900

    Update

--- Works3/WorkMauiMisc/WorkWifi/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkWifi/MainPage.xaml.cs
@@ -1,17 +1,55 @@
+using System.Diagnostics;
+
 namespace WorkWifi;
+
+using Android.Bluetooth;
+using Android.Content;
+using Android.Net.Wifi;
+
+using MauiComponents;
 
 public partial class MainPage : ContentPage
 {
+    private readonly AccessPointScanner accessPointScanner = new();
+
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnStartClick(object? sender, EventArgs e)
+    private async void OnStartClick(object? sender, EventArgs e)
     {
+        var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+        if (status != PermissionStatus.Granted)
+        {
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+            {
+                return;
+            }
+        }
+
+        Debug.WriteLine($"** {accessPointScanner.Test()}");
     }
 
     private void OnStopClick(object? sender, EventArgs e)
     {
+    }
+}
+
+public sealed class AccessPointScanner
+{
+    private readonly WifiManager wifiManager;
+
+
+    public AccessPointScanner()
+    {
+        var context = ActivityResolver.CurrentActivity.ApplicationContext!;
+        wifiManager = (WifiManager)context.GetSystemService(Context.WifiService)!;
+    }
+
+    public bool Test()
+    {
+        return wifiManager.StartScan();
     }
 }
--- Works3/WorkMauiMisc/WorkWifi/Platforms/Android/MainActivity.cs
+++ Works3/WorkMauiMisc/WorkWifi/Platforms/Android/MainActivity.cs
@@ -1,9 +1,16 @@
-﻿namespace WorkWifi;
+namespace WorkWifi;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
+using MauiWifiManager;
+
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        WifiNetworkService.Init(this);
+    }
 }
