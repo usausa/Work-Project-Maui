e8e8e065481d548b5801424677ff5202a91ee471
usausa <machi.pon@gmail.com>
2025-07-12 16:29:01 +0900

    Update

--- Works3/Template/Template.MobileApp/Modules/Basic/BasicConverterViewModel.cs
+++ Works3/Template/Template.MobileApp/Modules/Basic/BasicConverterViewModel.cs
@@ -1,7 +1,30 @@
 namespace Template.MobileApp.Modules.Basic;
 
-public sealed class BasicConverterViewModel : AppViewModelBase
+public sealed partial class BasicConverterViewModel : AppViewModelBase
 {
+    [ObservableProperty]
+    public partial bool ReverseChecked { get; set; }
+
+    [ObservableProperty]
+    public partial bool MultiChecked1 { get; set; }
+    [ObservableProperty]
+    public partial bool MultiChecked2 { get; set; }
+
+    [ObservableProperty]
+    public partial bool ToChecked { get; set; }
+
+    [ObservableProperty]
+    public partial string Text { get; set; }
+
+    public IObserveCommand SwitchTextCommand { get; }
+
+    public BasicConverterViewModel()
+    {
+        Text = string.Empty;
+
+        SwitchTextCommand = MakeDelegateCommand(() => Text = String.IsNullOrEmpty(Text) ? "Hello World" : string.Empty);
+    }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.BasicMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
