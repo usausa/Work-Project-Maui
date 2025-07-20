d1bc170c0ed5f36225c2944ad8ded340d0c44bdb
usausa <machi.pon@gmail.com>
2025-07-20 21:28:53 +0900

    Update

--- Works3/WorkMauiMisc/WorkVisualGauge/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkVisualGauge/MainPage.xaml.cs
@@ -4,13 +4,65 @@ using Microsoft.Maui.Graphics;
 
 public partial class MainPage : ContentPage, IDrawable
 {
-    public int Value { get; set; }
+    private const float StartAngle = 210f;
+    private const float EndAngle = -30f;
+    private const float RangeAngle = StartAngle - EndAngle;
+
+    // Value
+
+    public double Value { get; set; }
 
     public double Min { get; set; } = 20;
 
     public double Max { get; set; } = 120;
 
-    public double Threshold { get; set; } = 100;
+    public double Threshold { get; set; } = 80;
+
+    // Size
+
+    public float Margin { get; set; } = 12;
+
+    public float GaugeWidth { get; set; } = 4;
+
+    public float TickLength { get; set; } = 12;
+
+    public float MinorTickLength { get; set; } = 18;
+
+    public float MajorTickLength { get; set; } = 24;
+
+    public float TickWidth { get; set; } = 1;
+
+    public float MinorTickWidth { get; set; } = 2;
+
+    public float MajorTickWidth { get; set; } = 4;
+
+    public float TextOffset { get; set; } = 44;
+
+    public float NeedleWidth { get; set; } = 11;
+
+    public float CenterCircleRadius { get; set; } = 10;
+
+    public float NeedleLengthPercentage { get; set; } = 1f;
+
+    public float NeedleBackLengthPercentage { get; set; } = 0.15f;
+
+    // Font
+
+    public Font Font { get; set; } = new Font("Arial");
+
+    public float FontSize { get; set; } = 18;
+
+    // Color
+
+    public Color TextColor { get; set; } = Colors.White;
+
+    public Color GaugeColor { get; set; } = Colors.White;
+
+    public Color WarningGaugeColor { get; set; } = Colors.Red;
+
+    public Color NeedleColor { get; set; } = Colors.OrangeRed;
+
+    public Color NeedleCenterColor { get; set; } = Colors.Red;
 
     public MainPage()
     {
@@ -18,21 +70,24 @@ public partial class MainPage : ContentPage, IDrawable
 
         GraphicsView.Drawable = this;
 
-        //_ = RunTimerAsync();
-        Value = 45;
+        // Dummy
+        Value = Min;
+        _ = RunTimerAsync();
+        //Value = 0;
     }
 
+    // Dummy
     private async Task RunTimerAsync()
     {
         try
         {
-            using var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
+            using var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(200));
             while (await timer.WaitForNextTickAsync().ConfigureAwait(true))
             {
                 Value += 1;
-                if (Value > 100)
+                if (Value > 120)
                 {
-                    Value = 0;
+                    Value = 20;
                 }
 
                 GraphicsView.Invalidate();
@@ -45,105 +100,86 @@ public partial class MainPage : ContentPage, IDrawable
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
-        canvas.FillColor = Colors.Gray;
-        canvas.FillRectangle(dirtyRect);
+        var size = Math.Min(dirtyRect.Width, dirtyRect.Height) - Margin * 2;
+        var radius = size / 2;
+        var rect = new RectF(dirtyRect.Center.X - radius, Margin, size, size);
 
-        Draw2(canvas, new RectF(dirtyRect.X, dirtyRect.Y, dirtyRect.Width, dirtyRect.Height / 4));
-    }
+        var cx = dirtyRect.Center.X;
+        var cy = rect.Center.Y;
 
-    // TODO calc2
-    public void Draw2(ICanvas canvas, RectF dirtyRect)
-    {
-        canvas.FillColor = new Color(64, 64, 64);
-        canvas.FillRectangle(dirtyRect);
-        canvas.Antialias = true;
+        var range = Max - Min;
 
-        var margin = 12;
-        var tickSize = 24;
-
-        // dummy
-        canvas.FillColor = new Color(96, 96, 96);
-        canvas.FillRectangle(dirtyRect.Inflate(-margin, -margin));
-        canvas.FillColor = new Color(48, 48, 48);
-        canvas.FillRectangle(dirtyRect.Inflate(-(margin + tickSize), -(margin + tickSize)));
-        // dummy
-
-        var baseSize = Math.Min((dirtyRect.Width - ((margin + tickSize) * 2)) / 4, (dirtyRect.Height - ((margin + tickSize) * 2)));
-        //var baseTop= dirtyRect.Height - margin - baseSize;
-        //var baseHeight = dirtyRect.Height - baseTop - margin;
-        var baseTop = margin + tickSize;
-        var baseHeight = baseSize;
-
-        var sweepRect = new RectF(dirtyRect.X + margin + tickSize, baseTop, dirtyRect.Width - ((margin + tickSize) * 2), baseHeight);
-
-        canvas.FillColor = Colors.Black;
-        canvas.FillRectangle(sweepRect);
-
-        // dummy
-        canvas.FillColor = new Color(32, 32, 32);
-        canvas.FillRectangle(new RectF(sweepRect.X, sweepRect.Y + baseHeight, sweepRect.Width, sweepRect.Height));
-        canvas.FillColor = new Color(48, 48, 48);
-        canvas.FillRectangle(new RectF(sweepRect.X, sweepRect.Y + (baseHeight * 2), sweepRect.Width, sweepRect.Height));
-        canvas.FillRectangle(new RectF(sweepRect.X, sweepRect.Y + (baseHeight * 3), sweepRect.Width, sweepRect.Height));
-        // dummy
-
-        var cx = sweepRect.Center.X;
-        var cy = sweepRect.Y + (baseHeight * 2);
-        var radius = baseHeight * 2;
-        var arcRect = new RectF(cx - radius, cy - radius, radius * 2, radius * 2);
+        canvas.Font = Font;
+        canvas.FontSize = FontSize;
 
         // Tick
-        canvas.StrokeLineCap = LineCap.Round;
-        for (var i = 0; i <= 100; i += 10)
+        canvas.StrokeLineCap = LineCap.Square;
+        for (var i = 0; i <= (int)range; i++)
         {
-            var isMajor = i % 20 == 0;
-            var angle = (120f * i / 100) - 60;
-            var len = isMajor ? tickSize - 12 : tickSize - 16;
+            var angle = (float)((RangeAngle * i / range) - (StartAngle - 90));
+            var isMajor = i % 10 == 0;
+            var isMinor = i % 5 == 0;
+
+            var width = isMajor ? MajorTickWidth : isMinor ? MinorTickWidth : TickWidth;
+            var length = isMajor ? MajorTickLength : isMinor ? MinorTickLength : TickLength;
 
             canvas.SaveState();
 
             canvas.Translate(cx, cy);
             canvas.Rotate(angle);
 
-            canvas.StrokeColor = i >= 80 ? Colors.Red : Colors.White;
-            canvas.StrokeSize = isMajor ? 3 : 2;
-            canvas.DrawLine(0f, -radius, 0f, -radius - len);
+            canvas.StrokeColor = i + Min >= Threshold ? WarningGaugeColor : GaugeColor;
+            canvas.StrokeSize = width;
+            canvas.DrawLine(0, -radius + 0.5f, 0, -radius + length);
+
+            canvas.RestoreState();
 
             if (isMajor)
             {
-                canvas.FontColor = i >= 80 ? Colors.Red : Colors.White;
-                canvas.FontSize = 14;
-                canvas.DrawString($"{i}", 0, -radius - len - 4, HorizontalAlignment.Center);
-            }
+                var textOffset = radius - TextOffset;
+                var textRadian = (float)((StartAngle - (RangeAngle * i / range)) * MathF.PI / 180);
+                var textX = cx + textOffset * MathF.Cos(textRadian);
+                var textY = cy - textOffset * MathF.Sin(textRadian);
 
-            canvas.ResetState();
+                var text = $"{(int)(i + Min)}";
+                var textSize = canvas.GetStringSize(text, Font, FontSize);
+                canvas.FontColor = TextColor;
+                canvas.DrawString(text, textX, textY + (textSize.Height / 2), HorizontalAlignment.Center);
+            }
         }
 
         // Arc
-        canvas.StrokeSize = 3;
-        canvas.StrokeColor = Colors.White;
-        canvas.DrawArc(arcRect, 150f, 30f, true, false);
+        canvas.StrokeSize = GaugeWidth;
+        canvas.StrokeColor = GaugeColor;
+        canvas.DrawArc(rect, StartAngle, EndAngle, true, false);
 
-        var redStart = 150f - ((150f - 30f) * 80 / 100);
-        canvas.StrokeColor = Colors.Red;
-        canvas.DrawArc(arcRect, redStart, 30f, true, false);
+        var warningStart = StartAngle - (float)(RangeAngle * (Threshold - Min) / range);
+        canvas.StrokeColor = WarningGaugeColor;
+        canvas.DrawArc(rect, warningStart, EndAngle, true, false);
 
         // Needle
-        var valueAngle = (120f * Value / 100) - 60;
+        var needleRadian = (float)((StartAngle - (RangeAngle * (Value - Min) / range)) * MathF.PI / 180);
 
-        canvas.SaveState();
+        var needleLength = (radius - MajorTickLength) * NeedleLengthPercentage;
+        var needleBackLength = (radius - MajorTickLength) * NeedleBackLengthPercentage;
+        var baseCenterX = cx - (needleBackLength * MathF.Cos(needleRadian));
+        var baseCenterY = cy + (needleBackLength * MathF.Sin(needleRadian));
+        var baseAngle1 = needleRadian + (MathF.PI / 2);
+        var baseAngle2 = needleRadian - (MathF.PI / 2);
+        var baseRadius = NeedleWidth / 2;
 
-        canvas.ClipRectangle(dirtyRect.Inflate(-margin, -margin));
+        var path = new PathF();
+        path.MoveTo(cx + needleLength * MathF.Cos(needleRadian), cy - needleLength * MathF.Sin(needleRadian));
+        path.LineTo(baseCenterX + (baseRadius * MathF.Cos(baseAngle1)), baseCenterY - (baseRadius * MathF.Sin(baseAngle1)));
+        path.LineTo(baseCenterX + (baseRadius * MathF.Cos(baseAngle2)), baseCenterY - (baseRadius * MathF.Sin(baseAngle2)));
+        path.Close();
 
-        canvas.Translate(cx, cy);
-        canvas.Rotate(valueAngle);
+        canvas.FillColor = NeedleColor;
+        canvas.FillPath(path);
 
-        canvas.StrokeLineCap = LineCap.Round;
-        canvas.StrokeSize = 5;
-        canvas.StrokeColor = Colors.OrangeRed;
-
-        canvas.DrawLine(0, 0, 0, -radius - 20);
-
-        canvas.ResetState();
+        // Center
+        var centerCircleRadius = CenterCircleRadius;
+        canvas.FillColor = NeedleCenterColor;
+        canvas.FillCircle(cx, cy, centerCircleRadius);
     }
 }
