37063be0a2ddc360968876cadc92bfffd9f5a6ce
usausa <machi.pon@gmail.com>
2025-07-21 10:58:08 +0900

    Update

--- Works3/Template/Template.MobileApp/Controls/NoiseGauge.cs
+++ Works3/Template/Template.MobileApp/Controls/NoiseGauge.cs
@@ -4,7 +4,7 @@ using System;
 
 using Font = Microsoft.Maui.Graphics.Font;
 
-public class NoiseGauge : GraphicsView, IDrawable
+public sealed class NoiseGauge : GraphicsView, IDrawable
 {
     private const float StartAngle = 210f;
     private const float EndAngle = -30f;
