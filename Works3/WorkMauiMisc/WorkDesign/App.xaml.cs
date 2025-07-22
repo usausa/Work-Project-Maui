54f1965d4896b16eab61ea6dd9154577659f10b1
usausa <machi.pon@gmail.com>
2025-07-22 17:45:31 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/App.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/App.xaml.cs
@@ -10,15 +10,20 @@ public partial class App : Application
     // https://editor.method.ac/
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        //return new Window(new ButtonPage());
         //return new Window(new HeaderPage());
+        //return new Window(new ButtonPage());
         //return new Window(new PathPage());
+        // -----
+        return new Window(new ChatPage());
+        // -----
         //return new Window(new MailPage());
+        // -----
         //return new Window(new SchedulePage());
         //return new Window(new CalendarPage());
 
-        return new Window(new ActivityPage());
+        // -----
         //return new Window(new MainPage());
+        //return new Window(new ActivityPage());
         //return new Window(new LabelPage());
         //return new Window(new BadgePage());
         //return new Window(new StylePage());
