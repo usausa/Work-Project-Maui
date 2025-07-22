c2c627833aeab2c0d8fea255fa59ba6570d5be26
usausa <machi.pon@gmail.com>
2025-07-22 09:34:01 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkBottom/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace WorkBottom;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Works3/WorkMauiMisc/WorkDesign/ActivityPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace WorkDesign;
+
+public partial class ActivityPage : ContentPage
+{
+	public ActivityPage()
+	{
+		InitializeComponent();
+	}
+}
