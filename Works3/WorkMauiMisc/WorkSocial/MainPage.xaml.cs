c133623165b4081b17bf4610abffc726e1ec85dd
usausa <machi.pon@gmail.com>
2025-07-30 09:03:40 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkSocial/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace WorkSocial;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Works3/WorkMauiMisc/WorkSocial/MauiProgram.cs
@@ -0,0 +1,33 @@
+namespace WorkSocial;
+
+using Microsoft.Extensions.Logging;
+
+using SkiaSharp.Views.Maui.Controls.Hosting;
+
+using Syncfusion.Maui.Toolkit.Hosting;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseSkiaSharp()
+            .ConfigureSyncfusionToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("851Gkktt_005.ttf", "Gkktt");
+                fonts.AddFont("OpenSans-Regular.ttf", "NotoSerifJPMedium");
+                fonts.AddFont("OpenSans-Semibold.ttf", "NotoSerifJPRegular");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
