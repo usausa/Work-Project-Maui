88246dcb167e8507b21d35f6cff22c078fa53dc1
usausa <machi.pon@gmail.com>
2025-08-06 18:13:28 +0900

    Update

--- Works3/WorkMauiMisc/WorkCommunication/App.xaml.cs
+++ Works3/WorkMauiMisc/WorkCommunication/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace WorkCommunication;
+namespace WorkCommunication;
 
 public partial class App : Application
 {
@@ -9,6 +9,6 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AppShell());
+        return new Window(new MainPage());
     }
 }
--- Works3/WorkMauiMisc/WorkCommunication/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkCommunication/MainPage.xaml.cs
@@ -1,23 +1,70 @@
-﻿namespace WorkCommunication;
+namespace WorkCommunication;
+
+using static System.Net.Mime.MediaTypeNames;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
 
-    private void OnCounterClicked(object? sender, EventArgs e)
+    // TODO Wrapper
+
+    //  Add to AndroidManifest.xml
+    //
+    //  <queries>
+    //    <intent>
+    //      <action android:name="android.intent.action.DIAL" />
+    //      <data android:scheme="tel"/>
+    //    </intent>
+    //  </queries>
+    //  <queries>
+    //    <intent>
+    //      <action android:name="android.intent.action.VIEW" />
+    //      <data android:scheme="smsto"/>
+    //    </intent>
+    //  </queries>
+    //  <queries>
+    //    <intent>
+    //      <action android:name="android.intent.action.SENDTO" />
+    //      <data android:scheme="mailto" />
+    //    </intent>
+    //  </queries>
+
+    private void OnDialClicked(object? sender, EventArgs e)
     {
-        count++;
+        if (PhoneDialer.Default.IsSupported)
+        {
+            PhoneDialer.Default.Open("000-0000-0000");
+        }
+    }
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+    private async void OnSmsClicked(object? sender, EventArgs e)
+    {
+        if (Sms.Default.IsComposeSupported)
+        {
+            var text = "Hello world.";
+            var recipients = new List<string> { "000-0000-0000" };
+            var message = new SmsMessage(text, recipients);
+            await Sms.Default.ComposeAsync(message);
+        }
+    }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+    private async void OnEmailClicked(object? sender, EventArgs e)
+    {
+        if (Email.Default.IsComposeSupported)
+        {
+            var message = new EmailMessage
+            {
+                Subject = "Hi how are you?",
+                Body = "Thanks for being here, nice to meet you!",
+                BodyFormat = EmailBodyFormat.PlainText,
+                To = ["dummy@example.com"]
+            };
+            //message.Attachments.Add(new EmailAttachment(Path.Combine(FileSystem.CacheDirectory, "dotnet_bot.svg")));
+
+            await Email.Default.ComposeAsync(message);
+        }
     }
 }
