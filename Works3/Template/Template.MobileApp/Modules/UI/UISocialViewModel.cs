8a131e1e0c974b493faa3399bb62a05f5598c776
usausa <machi.pon@gmail.com>
2025-08-11 14:09:19 +0900

    Update

--- Works3/Template/Template.MobileApp/Modules/UI/UISocialViewModel.cs
+++ Works3/Template/Template.MobileApp/Modules/UI/UISocialViewModel.cs
@@ -2,6 +2,26 @@ namespace Template.MobileApp.Modules.UI;
 
 public sealed class UISocialViewModel : AppViewModelBase
 {
+    private readonly IScreen screen;
+
+    public UISocialViewModel(
+        IScreen screen)
+    {
+        this.screen = screen;
+    }
+
+    public override Task OnNavigatedToAsync(INavigationContext context)
+    {
+        screen.SetFullscreen(true);
+        return Task.CompletedTask;
+    }
+
+    public override Task OnNavigatingFromAsync(INavigationContext context)
+    {
+        screen.SetFullscreen(false);
+        return Task.CompletedTask;
+    }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.UIMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
