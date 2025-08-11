00f4d39263396092b76613df745c2445e8111bdd
usausa <machi.pon@gmail.com>
2025-08-11 10:28:18 +0900

    Update

--- Works3/WorkMauiMisc/WorkSplash/Platforms/Android/MainActivity.cs
+++ Works3/WorkMauiMisc/WorkSplash/Platforms/Android/MainActivity.cs
@@ -1,9 +1,45 @@
-﻿namespace WorkSplash;
+using AndroidX.Core.SplashScreen;
+
+namespace WorkSplash;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
+using static Android.Views.ViewTreeObserver;
+using static AndroidX.Core.SplashScreen.SplashScreen;
+
+[Activity(Theme = "@style/Theme.StartLogo", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity, IKeepOnScreenCondition, IOnExitAnimationListener
 {
+    private DateTime start;
+
+    // https://shapeshifter.design/
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        var splash = InstallSplashScreen(this);
+
+        base.OnCreate(savedInstanceState);
+
+        start = DateTime.Now;
+
+        //splash.SetKeepOnScreenCondition(this);
+        //splash.SetOnExitAnimationListener(this);
+    }
+
+    public bool ShouldKeepOnScreen()
+    {
+        //System.Diagnostics.Debug.WriteLine("* ShouldKeepOnScreen");
+        //if (DateTime.Now - start < TimeSpan.FromSeconds(3))
+        //{
+        //    // Wait for 2 seconds before showing the content
+        //    return false;
+        //}
+
+        return true;
+    }
+
+    public void OnSplashScreenExit(SplashScreenViewProvider splashScreenViewProvider)
+    {
+    }
 }
