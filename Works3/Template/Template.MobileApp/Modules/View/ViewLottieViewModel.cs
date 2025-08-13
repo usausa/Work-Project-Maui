9f0802be9e4007ee9340216a2cd26c20964829f8
usausa <machi.pon@gmail.com>
2025-08-13 09:13:49 +0900

    Update

--- Works3/Template/Template.MobileApp/Modules/View/ViewLottieViewModel.cs
+++ Works3/Template/Template.MobileApp/Modules/View/ViewLottieViewModel.cs
@@ -1,8 +1,29 @@
 namespace Template.MobileApp.Modules.View;
 
-public sealed class ViewLottieViewModel : AppViewModelBase
+public sealed partial class ViewLottieViewModel : AppViewModelBase
 {
+    [ObservableProperty]
+    public partial bool IsAnimationEnabled { get; set; }
+
+    [ObservableProperty]
+    public partial TimeSpan Duration { get; set; }
+
+    [ObservableProperty]
+    public partial TimeSpan Progress { get; set; }
+
     protected override Task OnNotifyBackAsync() => Navigator.ForwardAsync(ViewId.ViewMenu);
 
     protected override Task OnNotifyFunction1() => OnNotifyBackAsync();
+
+    protected override Task OnNotifyFunction3()
+    {
+        Progress = TimeSpan.Zero;
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnNotifyFunction4()
+    {
+        IsAnimationEnabled = !IsAnimationEnabled;
+        return Task.CompletedTask;
+    }
 }
