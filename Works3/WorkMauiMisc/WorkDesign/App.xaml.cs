89ef9c8ec49c6a05fd7de787e140d7073de58285
usausa <machi.pon@gmail.com>
2025-08-14 16:25:07 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/App.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/App.xaml.cs
@@ -12,8 +12,8 @@ public partial class App : Application
     // https://editor.method.ac/
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        return new Window(new AnimePage());
-        //return new Window(new CollectionPage());
+        //return new Window(new AnimePage());
+        return new Window(new CollectionPage());
         //return new Window(new MiscPage());
         //return new Window(new HeaderPage());
         //return new Window(new ButtonPage());
