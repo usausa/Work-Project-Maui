af971ac69e934270a580fd121aa421c5072c13a9
usausa <machi.pon@gmail.com>
2025-08-17 14:39:51 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/MailPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/MailPage.xaml.cs
@@ -1,17 +1,20 @@
-using Smart.Maui.Input;
-using Smart.Mvvm;
-
-using System.Runtime.CompilerServices;
-
 namespace WorkDesign;
 
-using Java.Time;
+using Android.Text;
+
+using Microsoft.Maui;
+
+using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
 
 using Smart.Maui.ViewModels;
+using Smart.Mvvm;
 
 using System.Collections.ObjectModel;
-using System.ComponentModel.Design;
 using System.Globalization;
+
+using static Microsoft.Maui.LifecycleEvents.AndroidLifecycle;
 
 public partial class MailPage : ContentPage
 {
@@ -146,5 +149,194 @@ public class MailDateTimeStringConverter : IValueConverter
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
+    }
+}
+
+public class TextToggle : SKCanvasView
+{
+    private static readonly SKTypeface Typeface = SKFontManager.Default.MatchCharacter('あ');
+
+    public event EventHandler<bool>? ToggleChanged;
+
+    private float animationProgress = 0f;
+
+    // State
+
+    public static readonly BindableProperty IsToggledProperty = BindableProperty.Create(
+        nameof(IsToggled),
+        typeof(bool),
+        typeof(TextToggle),
+        false,
+        propertyChanged: (b, o, n) => ((TextToggle)b).AnimateToggle((bool)n));
+
+    public bool IsToggled
+    {
+        get => (bool)GetValue(IsToggledProperty);
+        set => SetValue(IsToggledProperty, value);
+    }
+
+    // Text
+
+    public static readonly BindableProperty OnTextProperty = BindableProperty.Create(
+        nameof(OnText),
+        typeof(string),
+        typeof(TextToggle),
+        propertyChanged: Invalidate);
+
+    public string OnText
+    {
+        get => (string)GetValue(OnTextProperty);
+        set => SetValue(OnTextProperty, value);
+    }
+
+    public static readonly BindableProperty OffTextProperty = BindableProperty.Create(
+        nameof(OffText),
+        typeof(string),
+        typeof(TextToggle),
+        propertyChanged: Invalidate);
+
+    public string OffText
+    {
+        get => (string)GetValue(OffTextProperty);
+        set => SetValue(OffTextProperty, value);
+    }
+
+    // Font
+
+    public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(
+        nameof(FontSize),
+        typeof(double),
+        typeof(TextToggle),
+        14d,
+        propertyChanged: Invalidate);
+
+    public double FontSize
+    {
+        get => (double)GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty, value);
+    }
+
+    // Color
+
+    public static readonly BindableProperty SelectedTextColorProperty = BindableProperty.Create(
+        nameof(SelectedTextColor),
+        typeof(Color),
+        typeof(TextToggle),
+        Colors.Blue,
+        propertyChanged: Invalidate);
+
+    public Color SelectedTextColor
+    {
+        get => (Color)GetValue(SelectedTextColorProperty);
+        set => SetValue(SelectedTextColorProperty, value);
+    }
+
+    public static readonly BindableProperty SelectedBackgroundColorProperty = BindableProperty.Create(
+        nameof(SelectedBackgroundColor),
+        typeof(Color),
+        typeof(TextToggle),
+        Colors.White,
+        propertyChanged: Invalidate);
+
+    public Color SelectedBackgroundColor
+    {
+        get => (Color)GetValue(SelectedBackgroundColorProperty);
+        set => SetValue(SelectedBackgroundColorProperty, value);
+    }
+
+    public static readonly BindableProperty UnselectedTextColorProperty = BindableProperty.Create(
+        nameof(UnselectedTextColor),
+        typeof(Color),
+        typeof(TextToggle),
+        Colors.White,
+        propertyChanged: Invalidate);
+
+    public Color UnselectedTextColor
+    {
+        get => (Color)GetValue(UnselectedTextColorProperty);
+        set => SetValue(UnselectedTextColorProperty, value);
+    }
+
+    public static readonly BindableProperty UnselectedBackgroundColorProperty = BindableProperty.Create(
+        nameof(UnselectedBackgroundColor),
+        typeof(Color),
+        typeof(TextToggle),
+        Colors.LightGray,
+        propertyChanged: Invalidate);
+
+    public Color UnselectedBackgroundColor
+    {
+        get => (Color)GetValue(UnselectedBackgroundColorProperty);
+        set => SetValue(UnselectedBackgroundColorProperty, value);
+    }
+
+    public TextToggle()
+    {
+        EnableTouchEvents = true;
+        Touch += (_, _) =>
+        {
+            IsToggled = !IsToggled;
+            ToggleChanged?.Invoke(this, IsToggled);
+            AnimateToggle(IsToggled);
+        };
+    }
+
+    private static void Invalidate(BindableObject bindable, object oldValue, object newValue)
+    {
+        ((TextToggle)bindable).InvalidateSurface();
+    }
+
+    private void AnimateToggle(bool newState)
+    {
+        var start = animationProgress;
+        var end = newState ? 1f : 0f;
+        var animation = new Animation(v =>
+        {
+            animationProgress = (float)v;
+            InvalidateSurface();
+        }, start, end);
+
+        animation.Commit(this, "ToggleAnimation", 16, 200, Easing.SinInOut);
+    }
+
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+    {
+        var canvas = e.Surface.Canvas;
+        canvas.Clear();
+
+        using var paint = new SKPaint();
+        paint.IsAntialias = true;
+
+        var radius = e.Info.Height / 2f;
+
+        using var font = new SKFont(Typeface, size: (float)(FontSize * DeviceDisplay.MainDisplayInfo.Density));
+
+        var onTextWidth = font.MeasureText(OnText);
+        var offTextWidth = font.MeasureText(OffText);
+        var onWidth = e.Info.Width * onTextWidth / (onTextWidth + offTextWidth);
+        var offWidth = e.Info.Width - onWidth;
+
+        // Background
+        paint.Color = UnselectedBackgroundColor.ToSKColor();
+        canvas.DrawRoundRect(new SKRoundRect(e.Info.Rect, radius), paint);
+
+        // Selected background
+        var animStart = Lerp(0, onWidth, animationProgress);
+        var animWidth = Lerp(onWidth, offWidth, animationProgress);
+        paint.Color = SelectedBackgroundColor.ToSKColor();
+        canvas.DrawRoundRect(new SKRoundRect(new SKRect(animStart, e.Info.Rect.Top, animStart + animWidth, e.Info.Rect.Bottom), radius), paint);
+
+        // Unselected text
+        //paint.Color = UnselectedTextColor.ToSKColor();
+        paint.Color = SKColors.Black;
+        var onTextX = (onWidth - onTextWidth) / 2f;
+        var offTextX = onWidth + ((offWidth - offTextWidth) / 2f);
+        var textY = (e.Info.Height - font.Metrics.Ascent - font.Metrics.Descent) / 2f;
+        canvas.DrawText(OnText, onTextX, textY, font, paint);
+        canvas.DrawText(OffText, offTextX, textY, font, paint);
+
+        // TODO
+
+        float Lerp(float from, float to, float progress) => from + (to - from) * progress;
     }
 }
