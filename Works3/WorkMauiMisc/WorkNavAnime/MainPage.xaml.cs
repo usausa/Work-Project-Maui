c53bf77b2958d5561478a819cfaa723cf2dbf9a2
usausa <machi.pon@gmail.com>
2025-09-02 13:48:38 +0900

    Update

--- Works3/WorkMauiMisc/WorkNavAnime/MainPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkNavAnime/MainPage.xaml.cs
@@ -11,20 +11,17 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    private void OnPushClicked(object? sender, EventArgs e) => Navigate(new Page1(), new PushTransition());
+    private void OnPushClicked(object? sender, EventArgs e) => Navigate(new Page2(), new PushTransition());
 
-    private void OnPopClicked(object? sender, EventArgs e) => Navigate(new Page2(), new PopTransition());
+    private void OnPopClicked(object? sender, EventArgs e) => Navigate(new Page1(), new PopTransition());
 
-    private void OnOpenClicked(object? sender, EventArgs e) => Navigate(new Page1(), new DialogOpenTransition());
+    private void OnOpenClicked(object? sender, EventArgs e) => Navigate(new Page2(), new DialogOpenTransition());
 
-    private void OnCloseClicked(object? sender, EventArgs e) => Navigate(new Page2(), new DialogCloseTransition());
+    private void OnCloseClicked(object? sender, EventArgs e) => Navigate(new Page1(), new DialogCloseTransition());
 
-    private void OnUpClicked(object? sender, EventArgs e) => Navigate(new Page1(), new BottomUpTransition());
+    private void OnUpClicked(object? sender, EventArgs e) => Navigate(new Page2(), new BottomUpTransition());
 
-    private void OnDownClicked(object? sender, EventArgs e) => Navigate(new Page2(), new BottomDownTransition());
-
-    private const int CloseDuration = 100;
-    private const int OpenDuration = 250;
+    private void OnDownClicked(object? sender, EventArgs e) => Navigate(new Page1(), new BottomDownTransition());
 
     private async void Navigate(ContentView newPage, INavigationEffect effect)
     {
@@ -35,25 +32,31 @@ public partial class MainPage : ContentPage
 
         navigating = true;
 
-        if (currentPage != null)
-        {
-            var oldPage = currentPage;
-
-            await effect.AnimateOutAsync(oldPage);
-            Container.Children.Remove(oldPage);
-        }
+        var oldPage = currentPage;
 
         Container.Children.Add(newPage);
 
-        // 新しい画面の初期状態を設定
-        newPage.Opacity = 0;
-        newPage.TranslationX = 0;
-        newPage.TranslationY = 0;
-        newPage.Scale = 1;
+        // [MEMO] 実際の実装ではオーダーをどうするかは要検討
+        if (oldPage != null)
+        {
+            effect.ReOrder(newPage, oldPage);
+        }
 
-        await effect.AnimateInAsync(newPage);
+        var animationTasks = new List<Task>
+        {
+            effect.AnimateInAsync(newPage)
+        };
+        if (oldPage != null)
+        {
+            animationTasks.Add(effect.AnimateOutAsync(oldPage));
+        }
 
-        // Stackされるなら元画面もリセット
+        await Task.WhenAll(animationTasks);
+
+        if (oldPage != null)
+        {
+            Container.Children.Remove(oldPage);
+        }
 
         currentPage = newPage;
         navigating = false;
@@ -64,6 +67,8 @@ public partial class MainPage : ContentPage
 
 public interface INavigationEffect
 {
+    void ReOrder(ContentView newPage, ContentView oldPage);
+
     Task AnimateInAsync(ContentView page);
 
     Task AnimateOutAsync(ContentView page);
@@ -71,140 +76,154 @@ public interface INavigationEffect
 
 public class PushTransition : INavigationEffect
 {
-    private const int OpenDuration = 500;
-    private const int CloseDuration = 200;
+    private const int Duration = 250;
+
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        // Children.Add()のデフォルトの挙動を利用するため、Z-indexの調整は不要
+    }
 
     public Task AnimateInAsync(ContentView page)
     {
         page.TranslationX = page.Parent is View parentView ? parentView.Width : 0;
         return Task.WhenAll(
-            page.FadeTo(1, OpenDuration, Easing.CubicIn),
-            page.TranslateTo(0, 0, OpenDuration, Easing.CubicOut)
+            page.FadeTo(1, Duration, Easing.CubicIn),
+            page.TranslateTo(0, 0, Duration, Easing.CubicOut)
         );
     }
 
     public Task AnimateOutAsync(ContentView page)
     {
         return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.TranslateTo(-(page.Width / 4), 0, CloseDuration, Easing.CubicOut)
+            page.FadeTo(0, Duration, Easing.CubicIn),
+            page.TranslateTo(-(page.Width), 0, Duration, Easing.CubicOut)
         );
     }
 }
 
 public class PopTransition : INavigationEffect
 {
-    private const int OpenDuration = 500;
-    private const int CloseDuration = 200;
+    private const int Duration = 250;
+
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        oldPage.ZIndex = newPage.ZIndex + 1;
+    }
 
     public Task AnimateInAsync(ContentView page)
     {
         page.TranslationX = page.Parent is View parentView ? -parentView.Width : 0;
         return Task.WhenAll(
-            page.FadeTo(1, OpenDuration, Easing.CubicIn),
-            page.TranslateTo(0, 0, OpenDuration, Easing.CubicOut)
+            page.FadeTo(1, Duration, Easing.CubicIn),
+            page.TranslateTo(0, 0, Duration, Easing.CubicOut)
         );
     }
 
     public Task AnimateOutAsync(ContentView page)
     {
         return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.TranslateTo(page.Width / 4, 0, CloseDuration, Easing.CubicOut)
+            page.FadeTo(0, Duration, Easing.CubicIn),
+            page.TranslateTo(page.Width, 0, Duration, Easing.CubicOut)
         );
     }
 }
+
 public class DialogOpenTransition : INavigationEffect
 {
-    private const int OpenDuration = 500;
-    private const int CloseDuration = 200;
+    private const int Duration = 250;
+
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        // Z-indexの調整は不要
+    }
 
     public Task AnimateInAsync(ContentView page)
     {
         page.Scale = 0.8;
         return Task.WhenAll(
-            page.FadeTo(1, OpenDuration, Easing.CubicIn),
-            page.ScaleTo(1.0, OpenDuration, Easing.CubicOut)
+            page.FadeTo(1, Duration, Easing.CubicIn),
+            page.ScaleTo(1.0, Duration, Easing.CubicOut)
         );
     }
 
     public Task AnimateOutAsync(ContentView page)
     {
-        return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.ScaleTo(0.8, CloseDuration, Easing.CubicOut)
-        );
+        page.Opacity = 1;
+        page.TranslationY = 0;
+        return Task.CompletedTask;
     }
 }
 
 public class DialogCloseTransition : INavigationEffect
 {
-    //private const int OpenDuration = 250;
-    private const int CloseDuration = 200;
+    private const int Duration = 250;
 
-    // 画面を開く際のアニメーション (新しいページが出現)
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        oldPage.ZIndex = newPage.ZIndex + 1;
+    }
+
     public Task AnimateInAsync(ContentView page)
     {
-        // 閉じるアニメーションなので、このメソッドは通常使用されません
-        // 必要に応じて、何もしないか、あるいは瞬時に表示するロジックを実装
         page.Opacity = 1;
-        page.Scale = 1;
+        page.TranslationY = 0;
         return Task.CompletedTask;
     }
 
-    // 画面を閉じる際のアニメーション (現在のページが消える)
     public Task AnimateOutAsync(ContentView page)
     {
         return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.ScaleTo(0.8, CloseDuration, Easing.CubicOut) // 縮小しながら消える
+            page.FadeTo(0, Duration, Easing.CubicIn),
+            page.ScaleTo(0.8, Duration, Easing.CubicOut)
         );
     }
 }
 
 public class BottomUpTransition : INavigationEffect
 {
-    private const int OpenDuration = 250;
-    private const int CloseDuration = 100;
+    private const int Duration = 250;
+
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        // Z-indexの調整は不要
+    }
 
     public Task AnimateInAsync(ContentView page)
     {
         page.TranslationY = page.Parent is View parentView ? parentView.Height : 0;
         return Task.WhenAll(
-            page.FadeTo(1, OpenDuration, Easing.CubicIn),
-            page.TranslateTo(0, 0, OpenDuration, Easing.CubicOut)
+            page.FadeTo(1, Duration, Easing.CubicIn),
+            page.TranslateTo(0, 0, Duration, Easing.CubicOut)
         );
     }
 
     public Task AnimateOutAsync(ContentView page)
     {
-        return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.TranslateTo(0, page.Height, CloseDuration, Easing.CubicOut)
-        );
+        return page.FadeTo(0, Duration, Easing.CubicIn);
     }
 }
 
 public class BottomDownTransition : INavigationEffect
 {
-    //private const int OpenDuration = 250;
-    private const int CloseDuration = 100;
+    private const int Duration = 250;
 
-    // 画面を開く際のアニメーション (新しいページが出現)
+    public void ReOrder(ContentView newPage, ContentView oldPage)
+    {
+        oldPage.ZIndex = newPage.ZIndex + 1;
+    }
+
     public Task AnimateInAsync(ContentView page)
     {
-        // 閉じるアニメーションなので、このメソッドは通常使用されません
         page.Opacity = 1;
         page.TranslationY = 0;
         return Task.CompletedTask;
     }
 
-    // 画面を閉じる際のアニメーション (現在のページが消える)
     public Task AnimateOutAsync(ContentView page)
     {
         return Task.WhenAll(
-            page.FadeTo(0, CloseDuration, Easing.CubicIn),
-            page.TranslateTo(0, page.Parent is View parentView ? parentView.Height : 0, CloseDuration, Easing.CubicOut) // 下にスライドして消える
+            page.FadeTo(0, Duration, Easing.CubicIn),
+            page.TranslateTo(0, page.Parent is View parentView ? parentView.Height : 0, Duration, Easing.CubicOut)
         );
     }
 }
