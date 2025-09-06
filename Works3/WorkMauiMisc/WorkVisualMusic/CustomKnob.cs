fb99821b085ec0177c66363003fc37b4f07bdca9
usausa <machi.pon@gmail.com>
2025-09-06 13:59:31 +0900

    Update

--- Works3/WorkMauiMisc/WorkVisualMusic/CustomKnob.cs
+++ Works3/WorkMauiMisc/WorkVisualMusic/CustomKnob.cs
@@ -1,10 +1,60 @@
-﻿namespace WorkVisualMusic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace WorkVisualMusic;
 
-internal class CustomKnob
+public sealed class CustomKnob : GraphicsView, IDrawable
 {
+    // Value
+
+    public static readonly BindableProperty ValueProperty = BindableProperty.Create(
+        nameof(Value),
+        typeof(double),
+        typeof(CustomKnob),
+        0.0,
+        propertyChanged: (b, _, _) => ((CustomKnob)b).Invalidate());
+
+    public double Value
+    {
+        get => (double)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, Math.Clamp(value, Minimum, Maximum));
+    }
+
+    public static readonly BindableProperty MinimumProperty = BindableProperty.Create(
+        nameof(Minimum),
+        typeof(double),
+        typeof(CustomKnob),
+        0.0);
+
+    public double Minimum
+    {
+        get => (double)GetValue(MinimumProperty);
+        set => SetValue(MinimumProperty, value);
+    }
+
+    public static readonly BindableProperty MaximumProperty = BindableProperty.Create(
+        nameof(Maximum),
+        typeof(double),
+        typeof(CustomKnob),
+        100.0);
+
+    public double Maximum
+    {
+        get => (double)GetValue(MaximumProperty);
+        set => SetValue(MaximumProperty, value);
+    }
+
+    public CustomKnob()
+    {
+        Drawable = this;
+
+        var gesture = new PanGestureRecognizer();
+        gesture.PanUpdated += OnPanUpdated;
+        GestureRecognizers.Add(gesture);
+    }
+
+    private void OnPanUpdated(object? sender, PanUpdatedEventArgs e)
+    {
+    }
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+    }
 }
--- Works3/WorkMauiMisc/WorkVisualMusic/CustomSlider.cs
+++ Works3/WorkMauiMisc/WorkVisualMusic/CustomSlider.cs
@@ -1,10 +1,136 @@
 namespace WorkVisualMusic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-internal class CustomSlider
+using System;
+using System.Windows.Input;
+
+public class CustomSliderValueChangedEventArgs : EventArgs
 {
+    public double Value { get; }
+
+    public CustomSliderValueChangedEventArgs(double value)
+    {
+        Value = value;
+    }
+}
+
+public sealed class CustomSlider : GraphicsView, IDrawable
+{
+    // Value
+
+    public static readonly BindableProperty MinimumProperty = BindableProperty.Create(
+        nameof(Minimum),
+        typeof(double),
+        typeof(CustomSlider),
+        0.0,
+        propertyChanged: OnPropertyChanged);
+
+    public double Minimum
+    {
+        get => (double)GetValue(MinimumProperty);
+        set => SetValue(MinimumProperty, value);
+    }
+
+    public static readonly BindableProperty MaximumProperty = BindableProperty.Create(
+        nameof(Maximum),
+        typeof(double),
+        typeof(CustomSlider),
+        100.0,
+        propertyChanged: OnPropertyChanged);
+
+    public double Maximum
+    {
+        get => (double)GetValue(MaximumProperty);
+        set => SetValue(MaximumProperty, value);
+    }
+
+    public static readonly BindableProperty ValueProperty = BindableProperty.Create(
+        nameof(Value),
+        typeof(double),
+        typeof(CustomSlider),
+        0.0,
+        propertyChanged: OnValueChanged, defaultBindingMode: BindingMode.TwoWay,
+        coerceValue: CoerceValue);
+
+    public double Value
+    {
+        get => (double)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    // Event
+
+    public static readonly BindableProperty ValueChangedCommandProperty = BindableProperty.Create(
+        nameof(ValueChangedCommand),
+        typeof(ICommand),
+        typeof(CustomSlider));
+
+    public ICommand? ValueChangedCommand
+    {
+        get => (ICommand)GetValue(ValueChangedCommandProperty);
+        set => SetValue(ValueChangedCommandProperty, value);
+    }
+
+    public event EventHandler<CustomSliderValueChangedEventArgs>? ValueChanged;
+
+    public CustomSlider()
+    {
+        Drawable = this;
+
+        StartInteraction += OnStartInteraction;
+        DragInteraction += OnDragInteraction;
+        EndInteraction += OnEndInteraction;
+    }
+
+    private static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        ((CustomSlider)bindable).Invalidate();
+    }
+
+    private static void OnValueChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var slider = (CustomSlider)bindable;
+        var value = (double)newValue;
+
+        slider.ValueChanged?.Invoke(slider, new CustomSliderValueChangedEventArgs(value));
+
+        if (slider.ValueChangedCommand?.CanExecute(value) == true)
+        {
+            slider.ValueChangedCommand.Execute(value);
+        }
+
+        slider.Invalidate();
+    }
+
+    private static object CoerceValue(BindableObject bindable, object value)
+    {
+        var slider = (CustomSlider)bindable;
+        var doubleValue = (double)value;
+
+        if (doubleValue < slider.Minimum)
+            return slider.Minimum;
+
+        if (doubleValue > slider.Maximum)
+            return slider.Maximum;
+
+        return doubleValue;
+    }
+
+    private void OnStartInteraction(object? sender, TouchEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnDragInteraction(object? sender, TouchEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnEndInteraction(object? sender, TouchEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+    }
 }
