c9a7dff2187c2965608697e714db9403ce4afdec
usausa <machi.pon@gmail.com>
2025-09-07 22:21:19 +0900

    Update

--- Works3/WorkMauiMisc/WorkVisualMusic/SoundEqualizer.cs
+++ Works3/WorkMauiMisc/WorkVisualMusic/SoundEqualizer.cs
@@ -1,49 +1,15 @@
 namespace WorkVisualMusic;
 
-public sealed class SoundEqualizer : GraphicsView
+public sealed class SoundEqualizer : GraphicsView, IDrawable
 {
-    public static readonly BindableProperty LevelProperty =
-        BindableProperty.Create(nameof(Level), typeof(int), typeof(SoundEqualizer), 10, propertyChanged: RedrawControl);
+    // ------------------------------------------------------------
+    // Property
+    // ------------------------------------------------------------
+
+    // Setting
 
     public static readonly BindableProperty RangeProperty =
-        BindableProperty.Create(nameof(Range), typeof(int), typeof(SoundEqualizer), 10, propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty ValuesProperty =
-        BindableProperty.Create(nameof(Values), typeof(int[]), typeof(SoundEqualizer), Array.Empty<int>(), propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty StartColorProperty =
-        BindableProperty.Create(nameof(StartColor), typeof(Color), typeof(SoundEqualizer), Colors.DarkGreen, propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty EndColorProperty =
-        BindableProperty.Create(nameof(EndColor), typeof(Color), typeof(SoundEqualizer), Colors.LightGreen, propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty InactiveColorProperty =
-        BindableProperty.Create(nameof(InactiveColor), typeof(Color), typeof(SoundEqualizer), Colors.Gray, propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty HorizontalSpacingProperty =
-        BindableProperty.Create(nameof(HorizontalSpacing), typeof(float), typeof(SoundEqualizer), 2f, propertyChanged: RedrawControl);
-
-    public static readonly BindableProperty VerticalSpacingProperty =
-        BindableProperty.Create(nameof(VerticalSpacing), typeof(float), typeof(SoundEqualizer), 2f, propertyChanged: RedrawControl);
-
-    private static void RedrawControl(BindableObject bindable, object oldValue, object newValue)
-    {
-        if (bindable is SoundEqualizer control)
-        {
-            control.Invalidate();
-        }
-    }
-
-    public SoundEqualizer()
-    {
-        Drawable = new EqualizerDrawable(this);
-    }
-
-    public int Level
-    {
-        get => (int)GetValue(LevelProperty);
-        set => SetValue(LevelProperty, value);
-    }
+        BindableProperty.Create(nameof(Range), typeof(int), typeof(SoundEqualizer), 10, propertyChanged: OnPropertyChanged);
 
     public int Range
     {
@@ -51,23 +17,42 @@ public sealed class SoundEqualizer : GraphicsView
         set => SetValue(RangeProperty, value);
     }
 
+    public static readonly BindableProperty LevelProperty = BindableProperty.Create(
+        nameof(Level),
+        typeof(int),
+        typeof(SoundEqualizer),
+        10,
+        propertyChanged: OnPropertyChanged);
+
+    public int Level
+    {
+        get => (int)GetValue(LevelProperty);
+        set => SetValue(LevelProperty, value);
+    }
+
+    // Value
+
+    public static readonly BindableProperty ValuesProperty = BindableProperty.Create(
+        nameof(Values),
+        typeof(int[]),
+        typeof(SoundEqualizer),
+        Array.Empty<int>(),
+        propertyChanged: OnPropertyChanged);
+
     public int[] Values
     {
         get => (int[])GetValue(ValuesProperty);
         set => SetValue(ValuesProperty, value);
     }
 
-    public Color StartColor
-    {
-        get => (Color)GetValue(StartColorProperty);
-        set => SetValue(StartColorProperty, value);
-    }
+    // Color
 
-    public Color EndColor
-    {
-        get => (Color)GetValue(EndColorProperty);
-        set => SetValue(EndColorProperty, value);
-    }
+    public static readonly BindableProperty InactiveColorProperty = BindableProperty.Create(
+        nameof(InactiveColor),
+        typeof(Color),
+        typeof(SoundEqualizer),
+        Colors.Gray,
+        propertyChanged: OnPropertyChanged);
 
     public Color InactiveColor
     {
@@ -75,94 +60,120 @@ public sealed class SoundEqualizer : GraphicsView
         set => SetValue(InactiveColorProperty, value);
     }
 
-    public float HorizontalSpacing
+    public static readonly BindableProperty StartColorProperty = BindableProperty.Create(
+        nameof(StartColor),
+        typeof(Color),
+        typeof(SoundEqualizer),
+        Colors.DarkGreen,
+        propertyChanged: OnPropertyChanged);
+
+    public Color StartColor
     {
-        get => (float)GetValue(HorizontalSpacingProperty);
+        get => (Color)GetValue(StartColorProperty);
+        set => SetValue(StartColorProperty, value);
+    }
+
+    public static readonly BindableProperty EndColorProperty = BindableProperty.Create(
+        nameof(EndColor),
+        typeof(Color),
+        typeof(SoundEqualizer),
+        Colors.LightGreen,
+        propertyChanged: OnPropertyChanged);
+
+    public Color EndColor
+    {
+        get => (Color)GetValue(EndColorProperty);
+        set => SetValue(EndColorProperty, value);
+    }
+
+    // Size
+
+    public static readonly BindableProperty HorizontalSpacingProperty = BindableProperty.Create(
+        nameof(HorizontalSpacing),
+        typeof(double),
+        typeof(SoundEqualizer),
+        2d,
+        propertyChanged: OnPropertyChanged);
+
+    public double HorizontalSpacing
+    {
+        get => (double)GetValue(HorizontalSpacingProperty);
         set => SetValue(HorizontalSpacingProperty, value);
     }
 
-    public float VerticalSpacing
+    public static readonly BindableProperty VerticalSpacingProperty = BindableProperty.Create(
+        nameof(VerticalSpacing),
+        typeof(double),
+        typeof(SoundEqualizer),
+        2d,
+        propertyChanged: OnPropertyChanged);
+
+    public double VerticalSpacing
     {
-        get => (float)GetValue(VerticalSpacingProperty);
+        get => (double)GetValue(VerticalSpacingProperty);
         set => SetValue(VerticalSpacingProperty, value);
     }
 
-    private class EqualizerDrawable : IDrawable
+    // ------------------------------------------------------------
+    // Constructor
+    // ------------------------------------------------------------
+
+    public SoundEqualizer()
     {
-        private readonly SoundEqualizer _control;
+        Drawable = this;
+    }
 
-        public EqualizerDrawable(SoundEqualizer control)
+    // ------------------------------------------------------------
+    // Handler
+    // ------------------------------------------------------------
+
+    private static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        ((SoundEqualizer)bindable).Invalidate();
+    }
+
+    // ------------------------------------------------------------
+    // Draw
+    // ------------------------------------------------------------
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        if ((Level <= 0) || (Range <= 0))
         {
-            _control = control;
+            return;
         }
 
-        public void Draw(ICanvas canvas, RectF dirtyRect)
+        var horizontalSpacing = (float)HorizontalSpacing;
+        var verticalSpacing = (float)VerticalSpacing;
+
+        var totalHorizontalSpacing = horizontalSpacing * (Range - 1);
+        var totalVerticalSpacing = verticalSpacing * (Level - 1);
+
+        var cellWidth = (dirtyRect.Width - totalHorizontalSpacing) / Range;
+        var cellHeight = (dirtyRect.Height - totalVerticalSpacing) / Level;
+
+        var values = Values;
+        for (var i = 0; i < Range; i++)
         {
-            if (_control.Level <= 0 || _control.Range <= 0)
+            var value = (i < values.Length) ? Math.Clamp(values[i], 0, Level) : 0;
+            var x = i * (cellWidth + horizontalSpacing);
+
+            for (var j = 0; j < Level; j++)
             {
-                return;
-            }
+                var y = dirtyRect.Height - (j + 1) * cellHeight - j * verticalSpacing;
 
-            // Apply spacing to calculations
-            float horizontalSpacing = _control.HorizontalSpacing;
-            float verticalSpacing = _control.VerticalSpacing;
-
-            // Calculate available space for cells (excluding spacing)
-            float totalHorizontalSpacing = horizontalSpacing * (_control.Range - 1);
-            float totalVerticalSpacing = verticalSpacing * (_control.Level - 1);
-
-            // Calculate cell size (considering spacing)
-            float cellWidth = (dirtyRect.Width - totalHorizontalSpacing) / _control.Range;
-            float cellHeight = (dirtyRect.Height - totalVerticalSpacing) / _control.Level;
-
-            // Get values array
-            int[] values = _control.Values ?? Array.Empty<int>();
-
-            // Draw all Range columns, regardless of values array length
-            for (int i = 0; i < _control.Range; i++)
-            {
-                // Get value for this column, if available
-                int value = (i < values.Length) ? Math.Clamp(values[i], 0, _control.Level) : 0;
-
-                // Calculate the X position for this column
-                float x = i * (cellWidth + horizontalSpacing);
-
-                // Draw cells for this column
-                for (int j = 0; j < _control.Level; j++)
-                {
-                    // Calculate Y position for this cell (from bottom to top)
-                    float y = dirtyRect.Height - (j + 1) * cellHeight - j * verticalSpacing;
-
-                    // Determine if this cell should be active or inactive
-                    bool isActive = j < value;
-
-                    if (isActive)
-                    {
-                        // Calculate gradient color (j=0 is StartColor, j=Level-1 is EndColor)
-                        float factor = (float)j / (_control.Level - 1);
-                        Color gradientColor = InterpolateColor(_control.StartColor, _control.EndColor, factor);
-                        canvas.FillColor = gradientColor;
-                    }
-                    else
-                    {
-                        canvas.FillColor = _control.InactiveColor;
-                    }
-
-                    // Draw the cell
-                    canvas.FillRectangle(x, y, cellWidth, cellHeight);
-                }
+                canvas.FillColor = j < value ? InterpolateColor(StartColor, EndColor, (float)j / (Level - 1)) : InactiveColor;
+                canvas.FillRectangle(x, y, cellWidth, cellHeight);
             }
         }
+    }
 
-        // Helper method to interpolate between two colors
-        private Color InterpolateColor(Color startColor, Color endColor, float factor)
-        {
-            float r = startColor.Red + (endColor.Red - startColor.Red) * factor;
-            float g = startColor.Green + (endColor.Green - startColor.Green) * factor;
-            float b = startColor.Blue + (endColor.Blue - startColor.Blue) * factor;
-            float a = startColor.Alpha + (endColor.Alpha - startColor.Alpha) * factor;
-
-            return new Color(r, g, b, a);
-        }
+    private static Color InterpolateColor(Color c1, Color c2, float factor)
+    {
+        return new Color(
+            c1.Red + (c2.Red - c1.Red) * factor,
+            c1.Green + (c2.Green - c1.Green) * factor,
+            c1.Blue + (c2.Blue - c1.Blue) * factor,
+            c1.Alpha + (c2.Alpha - c1.Alpha) * factor);
     }
 }
