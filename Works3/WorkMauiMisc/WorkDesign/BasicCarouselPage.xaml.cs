7d415c322b8d4fb98e18ae46688413ecf79c8013
usausa <machi.pon@gmail.com>
2025-09-09 09:57:42 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/BasicCarouselPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/BasicCarouselPage.xaml.cs
@@ -62,7 +62,7 @@ public partial class BasicCarouselPage : ContentPage
 
 public class CarouselItem
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string ImageUrl { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
 }
--- Works3/WorkMauiMisc/WorkDesign/BasicSwipePage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/BasicSwipePage.xaml.cs
@@ -1,9 +1,35 @@
+using System.Diagnostics;
+
 namespace WorkDesign;
+
+using Smart.Maui.ViewModels;
+using Smart.Mvvm;
+
+using System.Windows.Input;
 
 public partial class BasicSwipePage : ContentPage
 {
-	public BasicSwipePage()
+    private BasicSwipePageViewModel TypedContext => (BasicSwipePageViewModel)BindingContext;
+
+    public BasicSwipePage()
 	{
 		InitializeComponent();
 	}
+}
+
+public sealed partial class BasicSwipePageViewModel : ExtendViewModelBase
+{
+    [ObservableProperty]
+    public partial List<BasicEntity> Results { get; set; } = [];
+
+    public ICommand DeleteCommand { get; }
+
+    public BasicSwipePageViewModel()
+    {
+        Results = BasicService.GetData().ToList();
+        DeleteCommand = MakeDelegateCommand<BasicEntity>(x =>
+        {
+            Debug.WriteLine($"* {x.Id} {x.Group} {x.Name}");
+        });
+    }
 }
