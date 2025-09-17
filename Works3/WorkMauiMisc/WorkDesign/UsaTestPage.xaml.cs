1051a53267c6f9dae56befd59f946794c39ac60c
usausa <machi.pon@gmail.com>
2025-09-17 09:37:43 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/UsaTestPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/UsaTestPage.xaml.cs
@@ -5,5 +5,7 @@ public partial class UsaTestPage : ContentPage
 	public UsaTestPage()
 	{
 		InitializeComponent();
-	}
+
+        CollectionView.ItemsSource = Enumerable.Range(1, 10).Select(x => new { Name = $"Item {x}" }).ToList();
+    }
 }
