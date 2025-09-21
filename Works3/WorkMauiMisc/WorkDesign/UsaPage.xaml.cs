3c17e05b1e73ddf121b33f1e6d4bafaa739a2a13
usausa <machi.pon@gmail.com>
2025-09-21 15:54:53 +0900

    Update

--- Works3/WorkMauiMisc/WorkDesign/UsaPage.xaml.cs
+++ Works3/WorkMauiMisc/WorkDesign/UsaPage.xaml.cs
@@ -9,6 +9,33 @@ public partial class UsaPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private object? lastSelected;
+
+    private void SelectableItemsView_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (sender is not CollectionView collectionView)
+        {
+            return;
+        }
+
+        var currentSelectedItem = collectionView.SelectedItem;
+
+        // 以前選択した項目と同じなら選択解除
+        if (lastSelected != null && Equals(lastSelected, currentSelectedItem))
+        {
+            collectionView.SelectedItem = null;
+            lastSelected = null;
+        }
+        else
+        {
+            lastSelected = currentSelectedItem;
+        }
+
+        var character = lastSelected as Character;
+        Image.Source = character?.Full;
+        Grid.IsVisible = character is not null;
+    }
 }
 
 public class UsaPageViewModel : ExtendViewModelBase
@@ -19,57 +46,57 @@ public class UsaPageViewModel : ExtendViewModelBase
     {
         Characters.Add(new Character
         {
-            Name = "Form 1",
-            Color = "#808080",
+            Name = "Ruler",
+            Color = Color.FromArgb("#81D4FA"),
             Face = "usa1_face.jpg",
             Full = "usa1_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 2",
-            Color = "#808080",
+            Name = "Caster",
+            Color = Color.FromArgb("#F48FB1"),
             Face = "usa2_face.jpg",
             Full = "usa2_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 3",
-            Color = "#808080",
+            Name = "Saber",
+            Color = Color.FromArgb("#80CBC4"),
             Face = "usa3_face.jpg",
             Full = "usa3_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 4",
-            Color = "#808080",
+            Name = "Berserker",
+            Color = Color.FromArgb("#B0BEC5"),
             Face = "usa4_face.jpg",
             Full = "usa4_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 5",
-            Color = "#808080",
+            Name = "Lancer",
+            Color = Color.FromArgb("#C5E1A5"),
             Face = "usa5_face.jpg",
             Full = "usa5_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 6",
-            Color = "#808080",
+            Name = "Rider",
+            Color = Color.FromArgb("#EF9A9A"),
             Face = "usa6_face.jpg",
             Full = "usa6_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 7",
-            Color = "#808080",
+            Name = "Assassin",
+            Color = Color.FromArgb("#B39DDB"),
             Face = "usa7_face.jpg",
             Full = "usa7_full.jpg"
         });
         Characters.Add(new Character
         {
-            Name = "Form 8",
-            Color = "#808080",
+            Name = "Alter Ego",
+            Color = Color.FromArgb("#EEEEEE"),
             Face = "usa8_face.jpg",
             Full = "usa8_full.jpg"
         });
@@ -80,7 +107,7 @@ public class Character
 {
     public string Name { get; set; } = default!;
 
-    public string Color { get; set; } = default!;
+    public Color Color { get; set; } = default!;
 
     public string Face { get; set; } = default!;
 
