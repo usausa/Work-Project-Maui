41379e240353245a59e2fb46468122c258810df7
usausa <machi.pon@gmail.com>
2025-10-16 13:40:37 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkPerfDebug/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WorkPerfDebug")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WorkPerfDebug.Pages")]
