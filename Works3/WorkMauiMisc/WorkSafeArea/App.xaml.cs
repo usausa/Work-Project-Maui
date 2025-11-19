9b2dcc26fd5fdf4cc2bac202beed2c86ce29ecab
usausa <machi.pon@gmail.com>
2025-11-19 15:38:32 +0900

    Update

---
+++ Works3/WorkMauiMisc/WorkSafeArea/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WorkSafeArea
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+            //return new Window(new MainPage());
+        }
+    }
+}
---
+++ Works3/WorkMauiMisc/WorkSafeArea/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkSafeArea
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
