ca31c6c4eee8b66d0f18ae35a795b1f2e052926d
usausa <machi.pon@gmail.com>
2025-12-08 14:47:27 +0900

    Add project

---
+++ Works3/WorkMauiMisc/WorkEntryBorder/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WorkEntryBorder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //Current!.UserAppTheme = AppTheme.Light;
+
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
